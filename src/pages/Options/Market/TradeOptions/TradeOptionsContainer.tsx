20b5ab578e8c7ddbd79344e157b96e0ac9ab25af
dgornjakovic <dgornjakovic87@gmail.com>
2021-03-29 17:53:31 +0200

    lose window.ethereum

--- src/pages/Options/Market/TradeOptions/TradeOptionsContainer.tsx
+++ src/pages/Options/Market/TradeOptions/TradeOptionsContainer.tsx
@@ -6,16 +6,16 @@ import { RootState } from 'redux/rootReducer';
 import { getNetworkId } from 'redux/modules/wallet';
 import TradeOptions from './TradeOptions';
 import { ContractWrappers0xProvider } from '../contexts/ContractWrappers0xContext';
+import snxJSConnector from '../../../../utils/snxJSConnector';
 
 declare const window: any;
 
 const TradeOptionsContainer: React.FC = () => {
     const [contractWrappers0x, setContractWrappers0x] = useState<ContractWrappers>();
     const networkId = useSelector((state: RootState) => getNetworkId(state));
-
     useEffect(() => {
         setContractWrappers0x(
-            new ContractWrappers(window.ethereum, {
+            new ContractWrappers((snxJSConnector.signer.provider as any).provider, {
                 chainId: networkId,
             })
         );
