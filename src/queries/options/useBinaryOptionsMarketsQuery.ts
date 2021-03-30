8722df05e398c8425b1797a0ac47409d3d1d1b90
dgornjakovic <dgornjakovic87@gmail.com>
2021-03-30 14:17:22 +0200

    move order fetching to cached API

--- src/queries/options/useBinaryOptionsMarketsQuery.ts
+++ src/queries/options/useBinaryOptionsMarketsQuery.ts
@@ -5,6 +5,7 @@ import { OptionsMarkets } from 'types/options';
 // import { getPhaseAndEndDate } from '../../utils/options';
 // import snxJSConnector from '../../utils/snxJSConnector';
 import { NetworkId } from 'utils/network';
+import { getPhaseAndEndDate } from '../../utils/options';
 
 const useBinaryOptionsMarketsQuery = (networkId: NetworkId, options?: UseQueryOptions<OptionsMarkets>) => {
     // const {
@@ -17,42 +18,14 @@ const useBinaryOptionsMarketsQuery = (networkId: NetworkId, options?: UseQueryOp
                 max: Infinity,
                 network: networkId,
             });
-            // for (const o of optionsMarkets) {
-            //     if ('trading' == getPhaseAndEndDate(o.biddingEndDate, o.maturityDate, o.expiryDate).phase) {
-            //         let baseUrl = 'https://api.0x.org/sra/v4/';
-            //         if (networkId == 3) {
-            //             baseUrl = 'https://ropsten.api.0x.org/sra/v4/';
-            //         }
-            //         let response = await fetch(
-            //             baseUrl + `orderbook?baseToken=` + o.longAddress + '&quoteToken=' + sUSD.contract.address
-            //         );
-            //         let responseJ = await response.json();
-            //         const totalLong = responseJ.bids.total + responseJ.asks.total;
-            //         o.orders = [];
-            //         if (responseJ.asks.records && responseJ.asks.records.length > 0) {
-            //             o.orders.push(responseJ.asks.records);
-            //         }
-            //         if (responseJ.bids.records && responseJ.bids.records.length > 0) {
-            //             o.orders.push(responseJ.bids.records);
-            //         }
-            //
-            //         response = await fetch(
-            //             baseUrl + `orderbook?baseToken=` + o.shortAddress + '&quoteToken=' + sUSD.contract.address
-            //         );
-            //         responseJ = await response.json();
-            //
-            //         if (responseJ.asks.records && responseJ.asks.records.length > 0) {
-            //             o.orders.push(responseJ.asks.records);
-            //         }
-            //         if (responseJ.bids.records && responseJ.bids.records.length > 0) {
-            //             o.orders.push(responseJ.bids.records);
-            //         }
-            //
-            //         const totalShort = responseJ.bids.total + responseJ.asks.total;
-            //
-            //         o.openOrders = totalLong + totalShort;
-            //     }
-            // }
+            for (const o of optionsMarkets) {
+                if ('trading' == getPhaseAndEndDate(o.biddingEndDate, o.maturityDate, o.expiryDate).phase) {
+                    const baseUrl = 'https://thales-api.herokuapp.com/options/' + networkId;
+                    const response = await fetch(baseUrl + '/' + o.address);
+                    const count = await response.text();
+                    o.openOrders = parseInt(count);
+                }
+            }
 
             return optionsMarkets;
         },
