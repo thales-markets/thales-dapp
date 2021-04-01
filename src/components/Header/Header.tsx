f47c3ba4fa3fcb25834ad09b4d50916a90b78b79
Gorshtak <sg31191@gmail.com>
2021-04-02 01:08:07 +0200

    header ui started

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,45 +1,93 @@
+import ROUTES from 'constants/routes';
+import { Link } from 'react-router-dom';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import ROUTES from '../../constants/routes';
-import { Button, Icon, Label, Menu, Message } from 'semantic-ui-react';
-import { RootState } from 'redux/rootReducer';
-import { getWalletInfo } from 'redux/modules/wallet';
-import onboardConnector from 'utils/onboardConnector';
-import { truncateAddress } from 'utils/formatters/string';
+import styled from 'styled-components';
+import logo from 'assets/images/logo.svg';
 
 const Header: React.FC = () => {
+    const Header = styled.div`
+        width: 100%;
+        display: flex;
+        height: 100px;
+        align-items: center;
+    `;
+
+    const Logo = styled(Link)`
+        display: flex;
+        flex: 1;
+        margin-left: 75px;
+        &:before {
+            content: ' ';
+            background-image: url(${logo});
+            height: 52px;
+            width: 52px;
+            margin-right: 20px;
+        }
+        font-family: Titillium Web;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 36px;
+        line-height: 55px;
+        color: white;
+    `;
+
+    const Links = styled.div`
+        flex: 3;
+        display: flex;
+        justify-content: flex-end;
+    `;
+
+    const NavLink = styled(Link)`
+        margin-right: 40px;
+        font-family: Titillium Web;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 16px;
+        line-height: 32px;
+        letter-spacing: 0.15px;
+        color: white;
+        &:last-child {
+            margin-right: 75px;
+        }
+    `;
+
+    const UsedApp = styled.button`
+        margin-left: 60px;
+        width: 140px;
+        height: 40px;
+        color: white;
+        background: #3936c7;
+        border-radius: 23px;
+        cursor: pointer;
+        border: none;
+        text-transform: none !important;
+        font-family: Titillium Web;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 16px;
+        line-height: 24px;
+        &:hover {
+            background: #44e1e2;
+        }
+    `;
+
     const { t } = useTranslation();
-    const { walletAddress, networkName } = useSelector((state: RootState) => getWalletInfo(state));
+    // const { walletAddress, networkName } = useSelector((state: RootState) => getWalletInfo(state));
 
     return (
-        <Menu>
-            <Link to={ROUTES.Home} className="item">
-                {t('header.links.home')}
-            </Link>
-            <Link to={ROUTES.Options.Home} className="item">
-                {t('header.links.options')}
-            </Link>
-            <Menu.Menu position="right">
-                {walletAddress != null ? (
-                    <>
-                        <Message style={{ margin: 0 }}>
-                            <Icon name="dot circle" color="green" />
-                            <span>{truncateAddress(walletAddress)}</span>
-                            <Label>{networkName}</Label>
-                        </Message>
-                        <Button secondary onClick={() => onboardConnector.disconnectWallet()}>
-                            {t('header.disconnect-wallet')}
-                        </Button>
-                    </>
-                ) : (
-                    <Button primary onClick={() => onboardConnector.connectWallet()}>
-                        {t('header.connect-wallet')}
-                    </Button>
-                )}
-            </Menu.Menu>
-        </Menu>
+        <Header>
+            <Logo to={ROUTES.Home}>{t('header.links.home')}</Logo>
+            <Links>
+                <NavLink to="">Products</NavLink>
+                <NavLink to="">Markets</NavLink>
+                <NavLink to="">Partners</NavLink>
+                <NavLink to="">Comunnity</NavLink>
+                <NavLink to={ROUTES.Options.Home}>
+                    <UsedApp>Used App</UsedApp>
+                </NavLink>
+            </Links>
+        </Header>
     );
 };
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,20 +3,25 @@ import styled from 'styled-components';
 import img1 from 'assets/images/img1.svg';
 import img2 from 'assets/images/img2.svg';
 import img3 from 'assets/images/img3.svg';
+import Header from 'components/Header';
 
 export const Home: React.FC = () => {
     const Section = styled.section`
         &:nth-child(odd) {
-            background: white;
-            #{}
+            background: linear-gradient(281.48deg, #04045a -16.58%, #141874 97.94%);
         }
         &:nth-child(even) {
-            background: linear-gradient(281.48deg, #04045a -16.58%, #141874 97.94%);
+            background: white;
         }
     `;
 
     const Wrapper = styled.div`
         display: flex;
+        width: min(100%, 1440px);
+        margin: auto;
+        @media (max-width: 768px) {
+            flex-direction: column;
+        }
     `;
 
     const Side = styled.div`
@@ -24,7 +29,13 @@ export const Home: React.FC = () => {
         display: flex;
         flex-direction: column;
         justify-content: center;
-        padding: 120px;
+        padding: 120px 140px 120px 60px;
+        &:first-child {
+            padding: 120px 60px 120px 140px;
+        }
+        @media (max-width: 768px) {
+            padding: 40px !important;
+        }
     `;
     const SideHeader = styled.h2`
         font-family: Titillium Web;
@@ -49,9 +60,36 @@ export const Home: React.FC = () => {
         height: 100%;
     `;
 
+    const ListHeader = styled.h2`
+        font-family: Titillium Web;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 25px;
+        line-height: 48px;
+        color: #fff;
+    `;
+
+    const List = styled.ul`
+        list-style-position: outside;
+        padding-left: 20px;
+    `;
+
+    const ListItem = styled.li`
+        font-family: Titillium Web;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 20px;
+        line-height: 32px;
+        letter-spacing: 0.2px;
+        color: #fff;
+    `;
+
     return (
         <>
             <Section>
+                <Wrapper>
+                    <Header />
+                </Wrapper>
                 <Wrapper>
                     <Side>
                         <SideHeader color="white">Non-custodial Options Trading</SideHeader>
@@ -79,10 +117,19 @@ export const Home: React.FC = () => {
                 <Wrapper>
                     <Side>
                         <SideHeader color="white">Trade Options</SideHeader>
-                        <SideContent color="white">
-                            Thales enables anyone to spin up a market to speculate on the prices of crypto assets,
-                            commodities, equities and index products - and earn fees for doing so.
-                        </SideContent>
+                        <ListHeader>Buy and Sell options</ListHeader>
+                        <List>
+                            <ListItem>
+                                Buy or sell a Yes or No option to respond to changes in the price of the underlying
+                            </ListItem>
+                            <ListItem>
+                                Identify high-probability opportunities to earn a profit on mispriced options
+                            </ListItem>
+                            <ListItem>
+                                Earn fees for being a market-maker who offers liquidity on Thales orderbooks, powered by
+                                0x
+                            </ListItem>
+                        </List>
                     </Side>
                     <Side>
                         <Image src={img3}></Image>
@@ -97,8 +144,13 @@ export const Home: React.FC = () => {
                     <Side>
                         <SideHeader color="#04045a">Who is Thales?</SideHeader>
                         <SideContent color="#04045a">
-                            Thales enables anyone to spin up a market to speculate on the prices of crypto assets,
-                            commodities, equities and index products - and earn fees for doing so.
+                            Thales is considered the first known person to exercise an options contract in Ancient
+                            Greece. In response to challengers who prodded Thales that if Thales was such a famous
+                            philosopher then how could he be impoverished, Thales leveraged his knowledge of the seasons
+                            and the stars into profitable actions. Thales bought the right to use all the olive presses
+                            in town at a very low price in the winter. Several months later, there was a bountiful olive
+                            harvest. Thales earned a killing renting out the olive presses to people willing to pay a
+                            much higher price than what Thales paid for the rights to use the olive presses.
                         </SideContent>
                     </Side>
                 </Wrapper>
