890e561db30400f5a37cd9352b8e03d23b5594aa
Vladan Poklopic <vladanpoklopic@gmail.com>
2021-04-05 13:46:13 +0200

    Add options icon on tabs

--- src/pages/Options/Market/TradeOptions/TradeOptions.tsx
+++ src/pages/Options/Market/TradeOptions/TradeOptions.tsx
@@ -1,23 +1,24 @@
 import React, { useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Menu } from 'semantic-ui-react';
+import OptionSideIcon from '../components/OptionSideIcon';
 import TradeOptionsSide from './TradeOptionsSide';
 
 const TradeOptions: React.FC = () => {
     const { t } = useTranslation();
     const tabContent: Array<{
-        id: 'long-options' | 'short-options';
+        id: 'long' | 'short';
         name: string;
         color: 'red' | 'green';
     }> = useMemo(
         () => [
             {
-                id: 'long-options',
+                id: 'long',
                 name: t('options.market.trade-options.trade-long-options-tab-title'),
                 color: 'green',
             },
             {
-                id: 'short-options',
+                id: 'short',
                 name: t('options.market.trade-options.trade-short-options-tab-title'),
                 color: 'red',
             },
@@ -38,12 +39,15 @@ const TradeOptions: React.FC = () => {
                         name={tab.id}
                         color={tab.color}
                     >
-                        {tab.name}
+                        {tab.name}{' '}
+                        <span style={{ marginLeft: 5, color: 'black' }}>
+                            <OptionSideIcon side={tab.id} />
+                        </span>
                     </Menu.Item>
                 ))}
             </Menu>
-            {activeTab.id === 'long-options' && <TradeOptionsSide optionSide="long" />}
-            {activeTab.id === 'short-options' && <TradeOptionsSide optionSide="short" />}
+            {activeTab.id === 'long' && <TradeOptionsSide optionSide="long" />}
+            {activeTab.id === 'short' && <TradeOptionsSide optionSide="short" />}
         </>
     );
 };
