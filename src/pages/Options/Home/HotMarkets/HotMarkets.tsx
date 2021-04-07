35e2674e5c9eb4caf6f77295e0e557495884eac7
Gorshtak <sg31191@gmail.com>
2021-04-07 03:47:21 +0200

    card design started

--- src/pages/Options/Home/HotMarkets/HotMarkets.tsx
+++ src/pages/Options/Home/HotMarkets/HotMarkets.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import TimeRemaining from '../../components/TimeRemaining';
-import { formatCurrencyWithSign } from 'utils/formatters/number';
-import { formatShortDate } from 'utils/formatters/date';
-import { USD_SIGN } from 'constants/currency';
-import { useTranslation } from 'react-i18next';
-import { navigateToOptionsMarket } from 'utils/routes';
 import { OptionsMarkets } from 'types/options';
-import MarketSentiment from '../../components/MarketSentiment';
+import { useTranslation } from 'react-i18next';
 import { FlexDivCentered, FlexDivColumn, SubTitle } from 'theme/common';
 import styled from 'styled-components';
-import Currency from 'components/Currency';
-import { Card } from 'semantic-ui-react';
+
+import MarketCard from '../MarketCard';
+import snxJSConnector from 'utils/snxJSConnector';
 
 type HotMarketsProps = {
     optionsMarkets: OptionsMarkets;
@@ -23,39 +18,15 @@ export const HotMarkets: React.FC<HotMarketsProps> = ({ optionsMarkets }) => {
         padding: 50px 110px;
     `;
 
+    console.log(snxJSConnector);
+
     return (
         <Wrapper>
             <SubTitle color="#04045a">{t('options.home.explore-markets.discover')}</SubTitle>
             <FlexDivCentered>
-                {optionsMarkets.map((optionsMarket) => {
-                    return (
-                        <Card
-                            key={optionsMarket.address}
-                            onClick={() => navigateToOptionsMarket(optionsMarket.address)}
-                        >
-                            <Card.Content>
-                                <Card.Header>
-                                    <Currency.Name
-                                        currencyKey={optionsMarket.currencyKey}
-                                        name={optionsMarket.asset}
-                                        showIcon={true}
-                                        iconProps={{ width: '24px', height: '24px', type: 'asset' }}
-                                    />
-                                    <TimeRemaining end={optionsMarket.timeRemaining} />
-                                </Card.Header>
-                                <Card.Description textAlign="center">
-                                    <div style={{ fontSize: 18, fontWeight: 600, marginBottom: 5, marginTop: 10 }}>
-                                        {formatCurrencyWithSign(USD_SIGN, optionsMarket.strikePrice)}
-                                    </div>
-                                    <div style={{ fontSize: 14, textTransform: 'uppercase' }}>
-                                        {t('common.by-date', { date: formatShortDate(optionsMarket.maturityDate) })}
-                                    </div>
-                                    <MarketSentiment long={optionsMarket.longPrice} short={optionsMarket.shortPrice} />
-                                </Card.Description>
-                            </Card.Content>
-                        </Card>
-                    );
-                })}
+                {optionsMarkets.map((optionsMarket, index) => (
+                    <MarketCard key={index} optionMarket={optionsMarket} />
+                ))}
             </FlexDivCentered>
         </Wrapper>
     );
