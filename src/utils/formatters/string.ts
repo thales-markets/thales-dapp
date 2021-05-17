2d2d1a8e920ed4e01475f0fb8ef538c957dd0e01
Vladan Poklopic <vladanpoklopic@gmail.com>
2021-05-17 13:15:30 +0200

    Update options price chart

--- src/utils/formatters/string.ts
+++ src/utils/formatters/string.ts
@@ -4,7 +4,7 @@ export const truncateAddress = (address: string, first = 5, last = 5) =>
     address ? `${address.slice(0, first)}...${address.slice(-last, address.length)}` : null;
 
 export const formatCurrencyPair = (baseCurrencyKey: CurrencyKey, quoteCurrencyKey: CurrencyKey) =>
-    `${baseCurrencyKey} / ${quoteCurrencyKey}`;
+    `${baseCurrencyKey}/${quoteCurrencyKey}`;
 
 export const strPadLeft = (string: string | number, pad: string, length: number) => {
     return (new Array(length + 1).join(pad) + string).slice(-length);
