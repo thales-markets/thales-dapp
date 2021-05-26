1e01f09196048deaad79b4b00412ee196178b277
Vladan Poklopic <vladanpoklopic@gmail.com>
2021-05-26 13:07:36 +0200

    Split charts

--- src/constants/ui.ts
+++ src/constants/ui.ts
@@ -6,7 +6,8 @@ export enum MarketWidgetKey {
     MATURITY_PHASE = 'maturity-phase-widget',
     ORDERBOOK = 'orderbook-widget',
     TRADE = 'trade-widget',
-    CHART = 'chart-widget',
+    CHART_TRADING_VIEW = 'chart-trading-view-widget',
+    CHART_OPTIONS = 'chart-options-widget',
     RECENT_TRANSACTIONS = 'recent-transactions-widget',
     YOUR_TRANSACTIONS = 'your-transactions-widget',
 }
@@ -17,7 +18,8 @@ export const MarketWidgetDefaultLayoutMap: Record<MarketWidgetKey, Layout> = {
     [MarketWidgetKey.MATURITY_PHASE]: { i: MarketWidgetKey.MATURITY_PHASE, x: 8, y: 0, w: 4, h: 13 },
     [MarketWidgetKey.ORDERBOOK]: { i: MarketWidgetKey.ORDERBOOK, x: 0, y: 1, w: 6, h: 17 },
     [MarketWidgetKey.TRADE]: { i: MarketWidgetKey.TRADE, x: 6, y: 1, w: 6, h: 17 },
-    [MarketWidgetKey.CHART]: { i: MarketWidgetKey.CHART, x: 0, y: 2, w: 12, h: 11 },
+    [MarketWidgetKey.CHART_TRADING_VIEW]: { i: MarketWidgetKey.CHART_TRADING_VIEW, x: 0, y: 2, w: 6, h: 11 },
+    [MarketWidgetKey.CHART_OPTIONS]: { i: MarketWidgetKey.CHART_OPTIONS, x: 6, y: 2, w: 6, h: 11 },
     [MarketWidgetKey.RECENT_TRANSACTIONS]: { i: MarketWidgetKey.RECENT_TRANSACTIONS, x: 0, y: 3, w: 12, h: 10 },
     [MarketWidgetKey.YOUR_TRANSACTIONS]: { i: MarketWidgetKey.YOUR_TRANSACTIONS, x: 0, y: 4, w: 12, h: 10 },
 };
