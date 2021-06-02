4f5737f9ba15c8a4637fa0ceededab1c621009e1
Gorshtak <sg31191@gmail.com>
2021-06-02 11:37:31 +0200

    fix error

--- src/pages/Options/Market/Market.tsx
+++ src/pages/Options/Market/Market.tsx
@@ -248,13 +248,6 @@ const Market: React.FC<MarketProps> = ({ marketAddress }) => {
     }, [isWalletConnected]);
 
     useEffect(() => {
-        return () => {
-            marketQuery.remove();
-            accountMarketInfoQuery.remove();
-        };
-    });
-
-    useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
 
