6a30e69187ff1e3291a670e44fa3711175b5fa36
Gorshtak <sg31191@gmail.com>
2021-06-03 03:31:45 +0200

    new galaxy on ladning page

--- src/pages/Home/Three.ts
+++ src/pages/Home/Three.ts
@@ -1,53 +1,79 @@
 import * as THREE from 'three';
 import { PerspectiveCamera } from 'three';
 
+const LOADER = new THREE.TextureLoader();
+
+const PARTICLES_CNT = 3000;
+const MIN_SPEED = 0.5;
+const MAX_SPEED = 10;
+const ACCELERATION = 0.1;
+const CAM_POSITION = 400;
+const STAR = LOADER.load('three/star.png');
+const SMOKE = LOADER.load('three/smoke.png');
+//const BACKGROUND = LOADER.load('background.png');
+
+let speedUp = false;
+const smoke_particles: THREE.Mesh<THREE.PlaneGeometry, THREE.MeshLambertMaterial>[] = [];
+let particleSpeed = MIN_SPEED;
+
 export const setupThreeJS = () => {
-    let speedUp = false;
-    let particleSpeed = 0.004;
     const heroHTML = document.getElementById('landing-hero');
     if (heroHTML) {
         const scene = new THREE.Scene();
+        const color = 0xffffff; // white
+        const near = 10;
+        const far = 100;
+        scene.fog = new THREE.Fog(color, near, far);
         const renderer = new THREE.WebGLRenderer();
         const camera = new PerspectiveCamera(100, heroHTML?.clientWidth / heroHTML?.clientHeight, 1, 1000);
-        camera.position.z = 400;
+        renderer.setClearColor('#04045a');
+        camera.position.z = CAM_POSITION;
         renderer.setSize(heroHTML?.clientWidth, heroHTML?.clientHeight);
-        const loader = new THREE.TextureLoader();
-        loader.load('background.png', function (texture) {
-            scene.background = texture;
-        });
         heroHTML.appendChild(renderer.domElement);
 
-        const particle = new THREE.Object3D();
+        const smokeGeo = new THREE.PlaneBufferGeometry(86, 93);
 
-        scene.add(particle);
-
-        const geometry = new THREE.TetrahedronGeometry(2, 0);
-
-        const material = new THREE.MeshPhongMaterial({
-            color: 0xffffff,
+        const smokeMaterial = new THREE.MeshLambertMaterial({
+            map: SMOKE,
+            transparent: true,
+            opacity: 0.03,
         });
 
-        for (let i = 0; i < 1000; i++) {
-            const mesh = new THREE.Mesh(geometry, material);
-            mesh.position.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5).normalize();
-            mesh.position.multiplyScalar(90 + Math.random() * 700);
-            mesh.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
-            particle.add(mesh);
+        for (let p = 0, l = 10000; p < l; p++) {
+            const particle = new THREE.Mesh(smokeGeo, smokeMaterial);
+
+            particle.position.set(
+                (Math.random() - 0.5) * (Math.random() * 2000),
+                (Math.random() - 0.5) * (Math.random() * 2000),
+                CAM_POSITION - 500 + Math.random() * 400
+            );
+            scene.add(particle);
+            smoke_particles.push(particle);
         }
 
-        const ambientLight = new THREE.AmbientLight(0x999999);
+        const particlesGeo = new THREE.BufferGeometry();
+        const posArr = new Float32Array(PARTICLES_CNT * 3);
+        const material = new THREE.PointsMaterial({
+            size: 10,
+            map: STAR,
+            color: '04045a',
+            transparent: true,
+            blending: THREE.AdditiveBlending,
+            alphaTest: 0.5,
+        });
+
+        for (let i = 0; i < PARTICLES_CNT * 3; i++) {
+            posArr[i] = (Math.random() - 0.5) * (Math.random() * 2000);
+        }
+
+        particlesGeo.setAttribute('position', new THREE.BufferAttribute(posArr, 3));
+
+        const particles = new THREE.Points(particlesGeo, material);
+        scene.add(particles);
+
+        const ambientLight = new THREE.AmbientLight(0x11e8bb);
         scene.add(ambientLight);
 
-        const lights = [];
-        lights[0] = new THREE.DirectionalLight(0xffffff, 1);
-        lights[0].position.set(1, 0, 0);
-        lights[1] = new THREE.DirectionalLight(0x11e8bb, 1);
-        lights[1].position.set(0.75, 1, 0.5);
-        lights[2] = new THREE.DirectionalLight(0x8200c9, 1);
-        lights[2].position.set(-0.75, -1, 0.5);
-        scene.add(lights[0]);
-        scene.add(lights[1]);
-        scene.add(lights[2]);
         const onWindowResize = () => {
             camera.aspect = heroHTML?.clientWidth / heroHTML?.clientHeight;
             camera.updateProjectionMatrix();
@@ -65,19 +91,22 @@ export const setupThreeJS = () => {
         });
 
         const animate = function () {
-            requestAnimationFrame(animate);
+            for (let i = 0; i < PARTICLES_CNT; i++) {
+                if (posArr[i * 3 + 2] >= CAM_POSITION) {
+                    posArr[i * 3 + 2] = -300;
+                }
+                posArr[i * 3 + 2] += particleSpeed;
+            }
+            particlesGeo.setAttribute('position', new THREE.BufferAttribute(posArr, 3));
 
             if (speedUp) {
-                particleSpeed = particleSpeed + 0.005 > 0.2 ? 0.2 : particleSpeed + 0.005;
+                particleSpeed = particleSpeed + ACCELERATION > MAX_SPEED ? MAX_SPEED : particleSpeed + ACCELERATION;
             } else {
-                particleSpeed = particleSpeed - 0.005 < 0.004 ? 0.004 : particleSpeed - 0.005;
+                particleSpeed = particleSpeed - ACCELERATION < MIN_SPEED ? MIN_SPEED : particleSpeed - ACCELERATION;
             }
-            particle.rotation.x -= 0.004;
-            particle.rotation.y -= particleSpeed;
-            particle.rotation.z -= 0.004;
-            renderer.clear();
 
             renderer.render(scene, camera);
+            requestAnimationFrame(animate);
         };
         animate();
     }
