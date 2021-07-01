5c93364b6eabe90130bfa661750e8a72a4ede3a4
Gorshtak <sg31191@gmail.com>
2021-07-01 14:47:51 +0200

    added thales-coin loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import { Image } from 'theme/common';
+import coin from 'assets/images/thales-coin.gif';
 import styled from 'styled-components';
 
 const Loader: React.FC = () => {
-    return <StyledLoader />;
+    return (
+        <Gif>
+            <Image src={coin}></Image>
+        </Gif>
+    );
 };
 
-const StyledLoader = styled(CircularProgress)`
-    position: absolute;
-    left: 50%;
-    top: 50%;
+const Gif = styled.div`
+    position: fixed;
+    height: 100%;
+    width: 100%;
+    background: #04045a;
 `;
 
 export default Loader;
