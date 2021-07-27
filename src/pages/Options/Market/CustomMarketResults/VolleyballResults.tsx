18b1e854cd69f925758feda98b28378c5f44fee8
Vladan Poklopic <vladanpoklopic@gmail.com>
2021-07-27 15:56:07 +0200

    Change URL for volleyball results

--- src/pages/Options/Market/CustomMarketResults/VolleyballResults.tsx
+++ src/pages/Options/Market/CustomMarketResults/VolleyballResults.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ScriptTag from 'react-script-tag';
 
 const VolleyballResults: React.FC = () => {
-    return <ScriptTag type="text/javascript" src="https://widget.enetscores.com/FW9472250CABC3433B"></ScriptTag>;
+    return <ScriptTag type="text/javascript" src="https://widget.enetscores.com/FWB28C8D00ACFBF0C7"></ScriptTag>;
 };
 
 export default VolleyballResults;
