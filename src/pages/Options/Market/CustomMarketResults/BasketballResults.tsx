d5f251ca4aeaf8274b26dc152696685d056451a0
Vladan Poklopic <vladanpoklopic@gmail.com>
2021-07-27 16:07:16 +0200

    Remove http for basketball results

--- src/pages/Options/Market/CustomMarketResults/BasketballResults.tsx
+++ src/pages/Options/Market/CustomMarketResults/BasketballResults.tsx
@@ -4,7 +4,7 @@ import Iframe from 'react-iframe';
 const BasketballResultsResults: React.FC = () => {
     return (
         <Iframe
-            url="http://www.fiba.basketball/fibacarousel/index.html?event=9562&amp;lng=en"
+            url="//www.fiba.basketball/fibacarousel/index.html?event=9562&amp;lng=en"
             width="100%"
             height="100%"
             id="basketball-results"
