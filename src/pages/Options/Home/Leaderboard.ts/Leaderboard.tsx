17671422996a540e8a420cd6e53ed53a89050d3e
Gorshtak <sg31191@gmail.com>
2021-08-09 12:54:43 +0200

    added sorting for leaderboard

--- src/pages/Options/Home/Leaderboard.ts/Leaderboard.tsx
+++ src/pages/Options/Home/Leaderboard.ts/Leaderboard.tsx
@@ -6,12 +6,16 @@ import { useSelector } from 'react-redux';
 import { getIsAppReady } from 'redux/modules/app';
 import { getNetworkId } from 'redux/modules/wallet';
 import { RootState } from 'redux/rootReducer';
-import { StyledTableCell, TableHeaderLabel } from '../MarketsTable/components';
+import { Arrow, ArrowsWrapper, StyledTableCell, TableHeaderLabel } from '../MarketsTable/components';
 import { PaginationWrapper, StyledTableRow } from '../MarketsTable/MarketsTable';
 import gold from 'assets/images/medals/gold.png';
 import silver from 'assets/images/medals/silver.png';
 import bronze from 'assets/images/medals/bronze.png';
 import leaderboardIcon from 'assets/images/medals/leaderboard.svg';
+import down from 'assets/images/down.svg';
+import up from 'assets/images/up.svg';
+import downSelected from 'assets/images/down-selected.svg';
+import upSelected from 'assets/images/up-selected.svg';
 import { Background, FlexDivColumnCentered, FlexDivRow, Image, MainWrapper } from 'theme/common';
 import { TableFooter } from '@material-ui/core';
 import Pagination from '../MarketsTable/Pagination';
@@ -28,9 +32,17 @@ const headCells: HeadCell[] = [
     { id: 2, label: '', sortable: false },
     { id: 3, label: 'Rank', sortable: false },
     { id: 4, label: 'Display Name', sortable: false },
-    { id: 5, label: 'Trades', sortable: false },
-    { id: 6, label: 'Volume', sortable: false },
+    { id: 5, label: 'Trades', sortable: true },
+    { id: 6, label: 'Volume', sortable: true },
 ];
+
+enum OrderDirection {
+    NONE,
+    ASC,
+    DESC,
+}
+
+const defaultOrderBy = 6; // Volume
 
 const LeaderboardPage: React.FC<any> = () => {
     const networkId = useSelector((state: RootState) => getNetworkId(state));
@@ -50,9 +62,32 @@ const LeaderboardPage: React.FC<any> = () => {
     const [rowsPerPage, setRowsPerPage] = React.useState(15);
     const numberOfPages = Math.ceil(leaderboard.length / rowsPerPage) || 1;
 
+    const [orderBy, setOrderBy] = useState(defaultOrderBy);
+    const [orderDirection, setOrderDirection] = useState(OrderDirection.DESC);
+
     const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
         setRowsPerPage(parseInt(event.target.value, 10));
         setPage(0);
+    };
+
+    const calcDirection = (cell: HeadCell) => {
+        if (orderBy === cell.id) {
+            switch (orderDirection) {
+                case OrderDirection.NONE:
+                    setOrderDirection(OrderDirection.DESC);
+                    break;
+                case OrderDirection.DESC:
+                    setOrderDirection(OrderDirection.ASC);
+                    break;
+                case OrderDirection.ASC:
+                    setOrderDirection(OrderDirection.DESC);
+                    setOrderBy(defaultOrderBy);
+                    break;
+            }
+        } else {
+            setOrderBy(parseInt(cell.id.toString()));
+            setOrderDirection(OrderDirection.DESC);
+        }
     };
 
     const memoizedPage = useMemo(() => {
@@ -66,8 +101,26 @@ const LeaderboardPage: React.FC<any> = () => {
         const searchLeader = leaderboard.filter((leader) => {
             return searchString === '' ? true : leader.walletAddress.includes(searchString);
         });
-        return searchLeader.slice(memoizedPage * rowsPerPage, rowsPerPage * (memoizedPage + 1));
-    }, [rowsPerPage, memoizedPage, searchString, leaderboard]);
+        return searchLeader.slice(memoizedPage * rowsPerPage, rowsPerPage * (memoizedPage + 1)).sort((a, b) => {
+            if (orderBy === 5) {
+                if (orderDirection === OrderDirection.DESC) {
+                    return b.trades - a.trades;
+                }
+                if (orderDirection === OrderDirection.ASC) {
+                    return a.trades - b.trades;
+                }
+            }
+            if (orderBy === 6) {
+                if (orderDirection === OrderDirection.DESC) {
+                    return b.volume - a.volume;
+                }
+                if (orderDirection === OrderDirection.ASC) {
+                    return a.volume - b.volume;
+                }
+            }
+            return 0;
+        });
+    }, [rowsPerPage, memoizedPage, searchString, leaderboard, orderBy, orderDirection]);
 
     return (
         <Background style={{ height: '100%', position: 'fixed', overflow: 'auto', width: '100%' }}>
@@ -96,10 +149,36 @@ const LeaderboardPage: React.FC<any> = () => {
                                         {headCells.map((cell: HeadCell, index) => {
                                             return (
                                                 <StyledTableCell
+                                                    onClick={cell.sortable ? calcDirection.bind(this, cell) : () => {}}
                                                     key={index}
                                                     style={cell.sortable ? { cursor: 'pointer' } : {}}
                                                 >
-                                                    <TableHeaderLabel>{cell.label}</TableHeaderLabel>
+                                                    <TableHeaderLabel
+                                                        className={
+                                                            cell.sortable && orderBy === cell.id ? 'selected' : ''
+                                                        }
+                                                    >
+                                                        {cell.label}
+                                                    </TableHeaderLabel>
+                                                    {cell.sortable && (
+                                                        <ArrowsWrapper>
+                                                            {orderBy === cell.id &&
+                                                            orderDirection !== OrderDirection.NONE ? (
+                                                                <Arrow
+                                                                    src={
+                                                                        orderDirection === OrderDirection.ASC
+                                                                            ? upSelected
+                                                                            : downSelected
+                                                                    }
+                                                                />
+                                                            ) : (
+                                                                <>
+                                                                    <Arrow src={up} />
+                                                                    <Arrow src={down} />
+                                                                </>
+                                                            )}
+                                                        </ArrowsWrapper>
+                                                    )}
                                                 </StyledTableCell>
                                             );
                                         })}
@@ -111,12 +190,15 @@ const LeaderboardPage: React.FC<any> = () => {
                                             <StyledTableRow key={index}>
                                                 <StyledTableCell></StyledTableCell>
                                                 <StyledTableCell>
-                                                    {page === 0 && index < 3 && (
-                                                        <Image
-                                                            src={getMedal(index)}
-                                                            style={{ width: 40, height: 40 }}
-                                                        ></Image>
-                                                    )}
+                                                    {page === 0 &&
+                                                        index < 3 &&
+                                                        orderDirection === OrderDirection.DESC &&
+                                                        orderBy === 6 && (
+                                                            <Image
+                                                                src={getMedal(index)}
+                                                                style={{ width: 40, height: 40 }}
+                                                            ></Image>
+                                                        )}
                                                 </StyledTableCell>
                                                 <StyledTableCell>{page * rowsPerPage + index + 1}</StyledTableCell>
                                                 <StyledTableCell>
