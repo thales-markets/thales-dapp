0c8f69d432151fe0847e7e05b331e93a58f2b8bf
Vladan Poklopic <vladanpoklopic@gmail.com>
2021-12-09 16:31:29 +0100

    Fix order book sorting

--- src/queries/options/useBinaryOptionsMarketOrderbook.ts
+++ src/queries/options/useBinaryOptionsMarketOrderbook.ts
@@ -5,6 +5,7 @@ import { NetworkId } from 'utils/network';
 import { prepBuyOrder, prepSellOrder } from 'utils/formatters/order';
 import { ORDERBOOK_AMOUNT_THRESHOLD } from 'constants/options';
 import { getAllBuyOrdersForToken, getAllSellOrdersForToken } from 'utils/1inch';
+import { orderBy } from 'lodash';
 
 const useBinaryOptionsMarketOrderbook = (
     networkId: NetworkId,
@@ -23,30 +24,38 @@ const useBinaryOptionsMarketOrderbook = (
             const sellOrders = await getAllSellOrdersForToken(networkId, optionsTokenAddress);
 
             if (sellOrders.length > 0) {
-                orderbook.sellOrders = sellOrders
-                    .map(
-                        (record: any): OrderItem => {
-                            return prepSellOrder(record);
-                        }
-                    )
-                    .filter(
-                        (order: OrderItem) =>
-                            order.displayOrder.fillableAmount >= ORDERBOOK_AMOUNT_THRESHOLD &&
-                            order.displayOrder.timeRemaining >= Date.now()
-                    );
+                orderbook.sellOrders = orderBy(
+                    sellOrders
+                        .map(
+                            (record: any): OrderItem => {
+                                return prepSellOrder(record);
+                            }
+                        )
+                        .filter(
+                            (order: OrderItem) =>
+                                order.displayOrder.fillableAmount >= ORDERBOOK_AMOUNT_THRESHOLD &&
+                                order.displayOrder.timeRemaining >= Date.now()
+                        ),
+                    'displayOrder.price',
+                    'asc'
+                );
             }
             if (buyOrders.length > 0) {
-                orderbook.buyOrders = buyOrders
-                    .map(
-                        (record: any): OrderItem => {
-                            return prepBuyOrder(record);
-                        }
-                    )
-                    .filter(
-                        (order: OrderItem) =>
-                            order.displayOrder.fillableAmount >= ORDERBOOK_AMOUNT_THRESHOLD &&
-                            order.displayOrder.timeRemaining >= Date.now()
-                    );
+                orderbook.buyOrders = orderBy(
+                    buyOrders
+                        .map(
+                            (record: any): OrderItem => {
+                                return prepBuyOrder(record);
+                            }
+                        )
+                        .filter(
+                            (order: OrderItem) =>
+                                order.displayOrder.fillableAmount >= ORDERBOOK_AMOUNT_THRESHOLD &&
+                                order.displayOrder.timeRemaining >= Date.now()
+                        ),
+                    'displayOrder.price',
+                    'desc'
+                );
             }
 
             return orderbook;
