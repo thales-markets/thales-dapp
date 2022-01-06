a9932275d1a211ca0737b45dc5e15fe5a5dc4772
vojin5rovic <vojinp13@gmail.com>
2022-01-06 13:09:09 +0100

    Thales Game - build 9

--- public/miletus-game/build.framework.js
+++ public/miletus-game/build.framework.js
@@ -1284,7 +1284,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2875632;
+STATICTOP = STATIC_BASE + 2880080;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3310,7 +3310,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2875632;
+var STATIC_BUMP = 2880080;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
