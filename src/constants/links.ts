c6a15943f1c609af5c388f073d968e877499c85d
Vladan Poklopic <vladanpoklopic@gmail.com>
2022-01-10 11:18:00 +0100

    TD-272 Update weekly rewards for SNX stakers and link to TIP-18

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -8,6 +8,8 @@ export const LINKS = {
             'https://docs.google.com/spreadsheets/d/1naboaD-UzIMHwT2bq7jNKkglA34jXmBTa2SrbWBNScg/edit#gid=0',
         RetroUnlockEligibilitySheet:
             'https://docs.google.com/spreadsheets/d/1nuTg7dL2BjMTbNAp_gnjjtXuhzNyw-DTy3PaZwsIXS0/edit#gid=0',
+        TIP18:
+            'https://thalesmarket.io/governance/thalesgov.eth/0x2b9f3fadf2bc92af007845b8d2e941e26428ddb965d24f985f7b969b87e58d9c',
     },
     AMM: {
         Tip: 'https://github.com/thales-markets/thales-improvement-proposals/blob/main/TIPs/TIP-11.md',
