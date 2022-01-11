76885ef4e3b7e06d2d8c7ae1d13a3a57bcc92a9b
Vojin Petrovic <vojinp13@gmail.com>
2022-01-11 15:39:50 +0100

    AMM Rewards - rework for round 2

--- src/pages/Options/AmmMining/AmmMining.tsx
+++ src/pages/Options/AmmMining/AmmMining.tsx
@@ -18,12 +18,16 @@ import AmmTable from './AmmTable';
 
 const DEFAULT_ORDER_BY = 2;
 
+export type Round2Trades = { long: number; short: number };
+
 const AmmMining: React.FC = () => {
     const { t } = useTranslation();
     const networkId = useSelector((state: RootState) => getNetworkId(state));
     const isAppReady = useSelector((state: RootState) => getIsAppReady(state));
     const [orderBy, setOrderBy] = useState(DEFAULT_ORDER_BY);
     const [orderDirection, setOrderDirection] = useState(OrderDirection.DESC);
+    const [selectedRound, setSelectedRound] = useState<number>(2);
+
     const { ammContract } = snxJSConnector;
     const ammContractAddress = ammContract ? ammContract.address : '';
 
@@ -32,26 +36,62 @@ const AmmMining: React.FC = () => {
     });
     const trades: ExtendedTrade[] = tradesQuery.isSuccess && tradesQuery.data ? tradesQuery.data : [];
 
-    const { dataForUI, volume } = useMemo(() => {
-        const map = new Map<string, number>();
+    const { dataForUI, volumePerRound, volumeByOptionSide, totalVolume, uniqueTraders } = useMemo(() => {
+        const map = new Map<string, number | Round2Trades>();
+        const uniqueTradersMap = new Map<string, boolean>();
+        let totalVolume = 0;
         let volume = 0;
-        if (trades.length > 0) {
-            trades.map((trade) => {
+        const volumeByOptionSide = { long: 0, short: 0 };
+        const activeTrades = trades.filter((trade) => {
+            if (trade.orderSide === 'buy' && trade.maker.toLowerCase() === ammContractAddress.toLowerCase()) {
+                totalVolume += trade.takerAmount;
+                uniqueTradersMap.set(trade.taker.toLowerCase(), true);
+            }
+            if (selectedRound === 1) {
+                return 1641730030000 > trade.marketItem.timestamp;
+            }
+            if (selectedRound === 2) {
+                return 1641730030000 < trade.marketItem.timestamp && 1643184000000 > trade.marketItem.timestamp;
+            }
+        });
+        if (activeTrades.length > 0) {
+            activeTrades.map((trade) => {
                 if (trade.orderSide === 'buy' && trade.maker.toLowerCase() === ammContractAddress.toLowerCase()) {
-                    if (!map.has(trade.taker.toLowerCase())) {
-                        map.set(trade.taker.toLowerCase(), 0);
-                    }
-                    const userVolume = map.get(trade.taker.toLowerCase());
+                    if (selectedRound === 1) {
+                        if (!map.has(trade.taker.toLowerCase())) {
+                            map.set(trade.taker.toLowerCase(), 0);
+                        }
+                        const userVolume = map.get(trade.taker.toLowerCase());
 
-                    map.set(trade.taker.toLowerCase(), (userVolume as any) + trade.takerAmount);
-                    volume += trade.takerAmount;
-                    return trade;
+                        map.set(trade.taker.toLowerCase(), (userVolume as any) + trade.takerAmount);
+                        volume += trade.takerAmount;
+                        return trade;
+                    } else if (selectedRound === 2) {
+                        if (!map.has(trade.taker.toLowerCase())) {
+                            map.set(trade.taker.toLowerCase(), { long: 0, short: 0 });
+                        }
+                        const userVolume = map.get(trade.taker.toLowerCase());
+
+                        map.set(trade.taker.toLowerCase(), {
+                            ...(userVolume as Round2Trades),
+                            [trade.optionSide]: (userVolume as Round2Trades)[trade.optionSide] + trade.takerAmount,
+                        });
+                        volumeByOptionSide[trade.optionSide] = volumeByOptionSide[trade.optionSide] + trade.takerAmount;
+                        volume += trade.takerAmount;
+                        return trade;
+                    }
                 }
             });
         }
 
-        return { dataForUI: Array.from(map), volume };
-    }, [trades]);
+        return {
+            dataForUI: Array.from(map),
+            volumePerRound: volume,
+            volumeByOptionSide,
+            totalVolume,
+            uniqueTraders: Array.from(uniqueTradersMap).length,
+        };
+    }, [trades, selectedRound]);
 
     return (
         <Background>
@@ -65,15 +105,12 @@ const AmmMining: React.FC = () => {
                 </Text>
                 <FlexDivColumn style={{ flex: 0 }}>
                     <Text className="text-sm lh24 ls5 pale-grey" style={{ marginBottom: 12 }}>
+                        <Trans i18nKey="options.amm-mining.subtitle1" />
+                    </Text>
+                    <Text className="text-sm lh24 ls5 pale-grey" style={{ marginBottom: 12 }}>
                         <Trans
-                            i18nKey="options.amm-mining.subtitle1"
+                            i18nKey="options.amm-mining.subtitle1a"
                             components={{
-                                a: (
-                                    <a
-                                        style={{ color: 'white', fontWeight: 'bold' }}
-                                        href="https://thalesmarket.io/markets"
-                                    />
-                                ),
                                 b: <strong />,
                             }}
                         />
@@ -85,7 +122,9 @@ const AmmMining: React.FC = () => {
                                 b: (
                                     <a
                                         style={{ color: 'white', fontWeight: 'bold' }}
-                                        href="https://thalesmarket.medium.com/thales-l2-deployment-2021-in-review-and-next-steps-975d2374d676"
+                                        href="https://thalesmarket.medium.com/round-2-of-thales-amm-markets-are-now-live-d09740d4ff22"
+                                        target="_blank"
+                                        rel="noreferrer"
                                     />
                                 ),
                             }}
@@ -101,21 +140,24 @@ const AmmMining: React.FC = () => {
 
                 <InfoContainer>
                     <Text className="text-sm pale-grey bold" style={{ marginRight: 20 }}>
-                        {`${t('options.leaderboard.trades.unique-traders')}: ${dataForUI.length}`}
+                        {`${t('options.leaderboard.trades.unique-traders')}: ${uniqueTraders}`}
                     </Text>
                     <Text className="text-sm pale-grey bold ">
-                        {`${t('options.leaderboard.trades.volume')}: ${formatCurrencyWithSign(USD_SIGN, volume)}`}
+                        {`${t('options.leaderboard.trades.volume')}: ${formatCurrencyWithSign(USD_SIGN, totalVolume)}`}
                     </Text>
                 </InfoContainer>
                 <AmmTable
                     dataForUi={dataForUI}
-                    volume={volume}
+                    volume={volumePerRound}
+                    volumeByOptionSide={volumeByOptionSide}
                     orderBy={orderBy}
                     orderDirection={orderDirection}
                     setOrderBy={setOrderBy}
                     setOrderDirection={setOrderDirection}
                     isLoading={tradesQuery.isLoading}
-                    deps={[orderBy, dataForUI, volume, orderDirection]}
+                    selectedRound={selectedRound}
+                    setSelectedRound={setSelectedRound}
+                    deps={[orderBy, dataForUI, volumePerRound, orderDirection]}
                 />
             </Wrapper>
         </Background>
