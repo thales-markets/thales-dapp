844e8fdc2ee4822055893e63e6a2bd295164fdc8
dmikic <dusan.mikic15@gmail.com>
2022-01-31 14:08:33 +0100

    TD-375 - link alignment on mobile devices fix

--- src/pages/V2/Home.tsx
+++ src/pages/V2/Home.tsx
@@ -170,6 +170,10 @@ const ThalesGame = styled(IconAbs)`
     @media (max-width: 600px) {
         font-size: 10em;
         line-height: 0.2em;
+    }
+    @media (max-width: 450px) {
+        font-size: 10em;
+        line-height: 0.2em;
         margin-bottom: 90px;
     }
 `;
