24b810b10b2472e7f8a53979a136c5ee5820e1e6
Vladan Poklopic <vladanpoklopic@gmail.com>
2022-02-01 15:55:11 +0100

    Update docs link

--- src/pages/Options/Earn/components/TokenOverview/TokenOverview.tsx
+++ src/pages/Options/Earn/components/TokenOverview/TokenOverview.tsx
@@ -113,7 +113,7 @@ export const TokentOverview: React.FC = () => {
                             </LightTooltip>
                             <LightTooltip title={t('options.earn.overview.earn-info-tooltip')}>
                                 <StyledLink
-                                    href="https://docs.thales.market/using-thales/thales+dodo-lp-rewards-guide"
+                                    href="https://docs.thalesmarket.io/thales-token/thales-+-dodo-lp-rewards-guide"
                                     target="_blank"
                                     rel="noreferrer"
                                 >
