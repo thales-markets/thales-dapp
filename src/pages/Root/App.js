bcd224eb9c890707eec64b94b58c74252b75cab7
Gorshtak <sg31191@gmail.com>
2022-02-01 15:36:10 +0100

    added console.log to check env variable

--- src/pages/Root/App.js
+++ src/pages/Root/App.js
@@ -57,6 +57,7 @@ const App = () => {
         const init = async () => {
             const { networkId, name } = await getEthereumNetwork();
             try {
+                console.log(process.env.REACT_APP_INFURA_PROJECT_ID);
                 dispatch(updateNetworkSettings({ networkId, networkName: name?.toLowerCase() }));
                 if (!snxJSConnector.initialized) {
                     const provider = loadProvider({
@@ -64,6 +65,7 @@ const App = () => {
                         infuraId: process.env.REACT_APP_INFURA_PROJECT_ID,
                         provider: window.ethereum,
                     });
+
                     const useOvm = getIsOVM(networkId);
 
                     snxJSConnector.setContractSettings({ networkId, provider, useOvm });
