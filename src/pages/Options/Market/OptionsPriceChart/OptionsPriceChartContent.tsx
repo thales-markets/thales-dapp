7c2900eb5715120c44e23920179bd1d3652ccbc4
makiBaraba <markovic.ofsec@gmail.com>
2022-02-17 18:41:49 +0100

    console.log clean

--- src/pages/Options/Market/OptionsPriceChart/OptionsPriceChartContent.tsx
+++ src/pages/Options/Market/OptionsPriceChart/OptionsPriceChartContent.tsx
@@ -64,8 +64,6 @@ const OptionsPriceChartContent: React.FC<OptionsPriceChartContentProps> = ({ opt
         return [];
     }, [tradesQuery.data]);
 
-    console.log('ChartData ', chartData);
-
     const isLoading = tradesQuery.isLoading;
     const noChartData = tradesQuery.isSuccess && chartData.length < 1;
 
