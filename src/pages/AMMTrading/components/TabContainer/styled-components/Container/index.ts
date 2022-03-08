5c17c7d2bdc816fa9c319f2d5c82975314651925
makiBaraba <markovic.ofsec@gmail.com>
2022-03-08 13:49:14 +0100

    TabMenu additional properties

--- src/pages/AMMTrading/components/TabContainer/styled-components/Container/index.ts
+++ src/pages/AMMTrading/components/TabContainer/styled-components/Container/index.ts
@@ -2,19 +2,25 @@ import styled, { StyledComponent } from 'styled-components';
 import { FlexDiv } from 'theme/common';
 
 type MenuContainerChild = {
-    Item: StyledComponent<'div', any, { active?: boolean }>;
+    Item: StyledComponent<
+        'div',
+        any,
+        { active?: boolean; customActiveColor?: string; noStrech?: boolean; padding?: string }
+    >;
 };
 
 type ContainerChild = {
-    Main: StyledComponent<'div', any> & MenuContainerChild;
+    Main: StyledComponent<'div', any, { justifyContent?: string }> & MenuContainerChild;
     Tab: StyledComponent<'div', any>;
 };
 
 // @ts-ignore
-const MenuContainer: StyledComponent<'div', any> & MenuContainerChild = styled(FlexDiv)`
+const MenuContainer: StyledComponent<'div', any, { justifyContent?: string }> & MenuContainerChild = styled(FlexDiv)<{
+    justifyContent?: string;
+}>`
     width: 100%;
     flex-direction: row;
-    justify-content: stretch;
+    justify-content: ${(_props) => (_props?.justifyContent ? _props.justifyContent : 'stretch')};
     align-items: stretch;
     border-bottom: 4px solid var(--table-border-color);
     border-radius: 3px;
@@ -31,15 +37,20 @@ const Tab = styled.div`
     display: flex;
 `;
 
-const MenuItem = styled.div<{ active?: boolean }>`
+const MenuItem = styled.div<{ active?: boolean; customActiveColor?: string; noStrech?: boolean; padding?: string }>`
     text-align: center;
-    flex: 1;
+    ${(_props) => (!_props?.noStrech ? 'flex: 1' : '')};
     font-family: Titillium Regular !important;
     font-style: normal;
     color: var(--primary-color);
-    box-shadow: ${(_props) => (_props?.active ? '0px 4px var(--primary-filter-menu-active)' : '')};
+    box-shadow: ${(_props) =>
+        _props?.active
+            ? _props?.customActiveColor
+                ? `${_props?.customActiveColor}`
+                : '0px 4px var(--primary-filter-menu-active)'
+            : ''};
     text-transform: uppercase;
-    padding: 12px 5px;
+    padding: ${(_props) => (_props?.padding ? _props.padding : '12px 5px')};
     cursor: pointer;
 `;
 
