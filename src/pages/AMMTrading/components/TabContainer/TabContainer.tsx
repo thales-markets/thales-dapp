4c9419f261a63c3ee5208ec7c59a7eab5b241dc1
makiBaraba <markovic.ofsec@gmail.com>
2022-03-19 00:42:31 +0100

    Similar Markets section

--- src/pages/AMMTrading/components/TabContainer/TabContainer.tsx
+++ src/pages/AMMTrading/components/TabContainer/TabContainer.tsx
@@ -1,15 +1,71 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import OptionPriceTab from '../Tabs/OptionPriceTab';
 import UserActivity from '../Tabs/UserActivity';
 import TradingView from '../Tabs/TradingView';
 import MarketActivity from '../Tabs/MarketActivity';
-
 import Container from './styled-components/Container';
+import SPAAnchor from 'components/SPAAnchor';
+import { SimilarMarketsContainer } from './styled-components/SimilarMarkets';
+
+import { fetchAllMarketOrders } from 'queries/options/fetchAllMarketOrders';
+import useBinaryOptionsMarketsQuery from 'queries/options/useBinaryOptionsMarketsQuery';
+import useExchangeRatesMarketDataQuery from 'queries/rates/useExchangeRatesMarketDataQuery';
+
+import { useSelector } from 'react-redux';
+import { RootState } from 'redux/rootReducer';
+import { getSimilarMarketsVisibility } from 'redux/modules/marketWidgets';
+import { getNetworkId } from 'redux/modules/wallet';
+import { sortOptionsMarkets } from 'utils/options';
+import { useMarketContext } from 'pages/AMMTrading/contexts/MarketContext';
+import { getIsAppReady } from 'redux/modules/app';
+import { buildOptionsMarketLink } from 'utils/routes';
+import MarketCard from 'pages/Markets/components/MarketsCard';
 
 const TabContainer: React.FC = () => {
+    const marketInfo = useMarketContext();
+
     const [currentTab, setCurrentTab] = useState<number>(0);
+    const similarMarketsVisibility = useSelector((state: RootState) => getSimilarMarketsVisibility(state));
+    const networkId = useSelector((state: RootState) => getNetworkId(state));
+    const isAppReady = useSelector((state: RootState) => getIsAppReady(state));
+    const marketsQuery = useBinaryOptionsMarketsQuery(networkId);
+    const openOrdersQuery = fetchAllMarketOrders(networkId);
+
+    const openOrdersMap = useMemo(() => {
+        if (openOrdersQuery.isSuccess) {
+            return openOrdersQuery.data;
+        }
+    }, [openOrdersQuery]);
+
+    const optionsMarkets = useMemo(() => {
+        if (marketsQuery.isSuccess && Array.isArray(marketsQuery.data)) {
+            let markets = openOrdersMap
+                ? marketsQuery.data.map((m) => ({
+                      ...m,
+                      openOrders: (openOrdersMap as any).get(m.address.toLowerCase())?.ordersCount ?? '0',
+                      availableLongs: (openOrdersMap as any).get(m.address.toLowerCase())?.availableLongs ?? '0',
+                      availableShorts: (openOrdersMap as any).get(m.address.toLowerCase())?.availableShorts ?? '0',
+                      longPrice: (openOrdersMap as any).get(m.address.toLowerCase())?.longPrice ?? '0',
+                      shortPrice: (openOrdersMap as any).get(m.address.toLowerCase())?.shortPrice ?? '0',
+                  }))
+                : marketsQuery.data;
+
+            markets = markets.filter((market) => market.currencyKey == marketInfo?.currencyKey);
+            markets = markets.filter((market) => market.availableLongs > 0 && market.availableShorts > 0);
+            markets = sortOptionsMarkets(markets);
+            return markets.slice(0, 6);
+        }
+        return [];
+    }, [marketsQuery]);
+
+    const exchangeRatesMarketDataQuery = useExchangeRatesMarketDataQuery(networkId, optionsMarkets, {
+        enabled: isAppReady && optionsMarkets.length > 0,
+        refetchInterval: false,
+    });
+    const exchangeRates = exchangeRatesMarketDataQuery.isSuccess ? exchangeRatesMarketDataQuery.data ?? null : null;
+
     const { t } = useTranslation();
 
     const tabItems = [
@@ -28,28 +84,51 @@ const TabContainer: React.FC = () => {
     ];
 
     return (
-        <Container>
-            <Container.Main>
-                {tabItems &&
-                    tabItems.map((item, index) => {
-                        return (
-                            <Container.Main.Item
-                                active={index == currentTab}
-                                key={index}
-                                onClick={() => setCurrentTab(index)}
-                            >
-                                {item.title}
-                            </Container.Main.Item>
-                        );
-                    })}
-            </Container.Main>
-            <Container.Tab>
-                {currentTab == 0 && <OptionPriceTab />}
-                {currentTab == 1 && <UserActivity />}
-                {currentTab == 2 && <TradingView />}
-                {currentTab == 3 && <MarketActivity />}
-            </Container.Tab>
-        </Container>
+        <>
+            {!similarMarketsVisibility && (
+                <Container>
+                    <Container.Main>
+                        {tabItems &&
+                            tabItems.map((item, index) => {
+                                return (
+                                    <Container.Main.Item
+                                        active={index == currentTab}
+                                        key={index}
+                                        onClick={() => setCurrentTab(index)}
+                                    >
+                                        {item.title}
+                                    </Container.Main.Item>
+                                );
+                            })}
+                    </Container.Main>
+                    <Container.Tab>
+                        {currentTab == 0 && <OptionPriceTab />}
+                        {currentTab == 1 && <UserActivity />}
+                        {currentTab == 2 && <TradingView />}
+                        {currentTab == 3 && <MarketActivity />}
+                    </Container.Tab>
+                </Container>
+            )}
+            {similarMarketsVisibility && (
+                <SimilarMarketsContainer>
+                    {optionsMarkets?.length > 0 &&
+                        optionsMarkets.map((optionMarket, index) => {
+                            return (
+                                <SPAAnchor key={index} href={buildOptionsMarketLink(optionMarket.address)}>
+                                    <MarketCard
+                                        optionMarket={optionMarket}
+                                        exchangeRates={exchangeRates}
+                                        marketCardStyle={{
+                                            maxWidth: '49%',
+                                            wrapperMargin: '0px 0px 10px 0px',
+                                        }}
+                                    />{' '}
+                                </SPAAnchor>
+                            );
+                        })}
+                </SimilarMarketsContainer>
+            )}
+        </>
     );
 };
 
