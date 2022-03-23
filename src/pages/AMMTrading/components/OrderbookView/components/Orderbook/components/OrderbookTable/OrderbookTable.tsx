140a8b0f68c57837ee91a45bcbec79e6650def28
makiBaraba <markovic.ofsec@gmail.com>
2022-03-23 22:59:43 +0100

    Orderbook Table component

---
+++ src/pages/AMMTrading/components/OrderbookView/components/Orderbook/components/OrderbookTable/OrderbookTable.tsx
@@ -0,0 +1,181 @@
+import React, { useMemo, DependencyList } from 'react';
+import { useTable, useSortBy, Column, Row } from 'react-table';
+import SimpleLoader from 'components/SimpleLoader';
+import { FlexDiv, FlexDivCentered } from 'theme/common';
+import styled from 'styled-components';
+import { OrderItem, OrderSide } from 'types/options';
+
+const COLORS = {
+    SELL: 'linear-gradient(89.92deg, #870000 -7.02%, #190A05 130.76%);',
+    BUY: 'linear-gradient(90deg, #50CE99 0%, #00C2C2 100%);',
+};
+
+type ColumnWithSorting<D extends Record<string, unknown>> = Column<D> & {
+    sortType?: string;
+    sortable?: boolean;
+};
+
+type OrderbookTableProps = {
+    data: Record<string, unknown>[];
+    columns: ColumnWithSorting<Record<string, unknown>>[];
+    columnsDeps?: DependencyList;
+    options?: any;
+    onTableRowClick?: (row: Row<any>) => void;
+    isLoading?: boolean;
+    noResultsMessage?: React.ReactNode;
+    orderSide: OrderSide;
+};
+
+const OrderbookTable: React.FC<OrderbookTableProps> = ({
+    columns = [],
+    columnsDeps = [],
+    data = [],
+    options = {},
+    noResultsMessage = null,
+    onTableRowClick = undefined,
+    isLoading = false,
+    orderSide,
+}) => {
+    const memoizedColumns = useMemo(() => columns, columnsDeps);
+    const { getTableProps, getTableBodyProps, rows, prepareRow } = useTable(
+        {
+            columns: memoizedColumns,
+            data,
+            ...options,
+        },
+        useSortBy
+    );
+
+    return (
+        <ReactTable {...getTableProps()}>
+            {isLoading ? (
+                <SimpleLoader />
+            ) : noResultsMessage != null ? (
+                <NoResultContainer>{noResultsMessage}</NoResultContainer>
+            ) : (
+                <TableBody {...getTableBodyProps()} orderSide={orderSide}>
+                    {rows.map((row, rowIndex: any) => {
+                        prepareRow(row);
+
+                        return (
+                            <TableRowBody
+                                {...row.getRowProps()}
+                                onClick={onTableRowClick ? () => onTableRowClick(row) : undefined}
+                                key={rowIndex}
+                            >
+                                <InnerTableRow
+                                    width={(row.original as OrderItem).displayOrder.percentageOfMaximum ?? 0}
+                                    orderSide={orderSide}
+                                />
+                                {row.cells.map((cell, cellIndex: any) => (
+                                    <TableCell
+                                        {...cell.getCellProps()}
+                                        key={cellIndex}
+                                        style={{ width: cell.column.width }}
+                                        justifyContent={
+                                            cell.column.id === 'displayOrder.timeRemaining' ? 'center' : 'left'
+                                        }
+                                    >
+                                        {cell.render('Cell')}
+                                    </TableCell>
+                                ))}
+                            </TableRowBody>
+                        );
+                    })}
+                </TableBody>
+            )}
+        </ReactTable>
+    );
+};
+
+const ReactTable = styled.div`
+    width: 100%;
+    height: 100%;
+    overflow-x: auto;
+    position: relative;
+    display: flex;
+`;
+
+const TableBody = styled.div<{ orderSide: OrderSide }>`
+    display: flex;
+    flex-direction: ${(props) => (props.orderSide === 'buy' ? 'column' : 'column-reverse')};
+    overflow: auto;
+    ::-webkit-scrollbar {
+        width: 5px;
+    }
+    ::-webkit-scrollbar-track {
+        background: #04045a;
+    }
+    ::-webkit-scrollbar-thumb {
+        border-radius: 15px;
+        background: #355dff;
+    }
+    ::-webkit-scrollbar-thumb:active {
+        background: #44e1e2;
+    }
+    ::-webkit-scrollbar-thumb:hover {
+        background: rgb(67, 116, 255);
+    }
+`;
+
+export const TableRow = styled(FlexDiv)`
+    min-height: 32px;
+    height: 32px;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 16px;
+    letter-spacing: 0.25px;
+    color: #f6f6fe;
+    position: relative;
+    @media (max-width: 512px) {
+        font-size: 10px;
+    }
+`;
+
+export const TableRowBody = styled(TableRow)`
+    &:hover {
+        cursor: pointer;
+        /* background: #116; */
+    }
+`;
+
+const InnerTableRow = styled(FlexDiv)<{ width: number; orderSide: OrderSide }>`
+    min-height: 32px;
+    height: 32px;
+    background: ${(props) => (props.orderSide === 'buy' ? COLORS.BUY : COLORS.SELL)};
+    mix-blend-mode: normal;
+    opacity: 0.8;
+    &:hover {
+        opacity: 1;
+    }
+    width: ${(props) => `${props.width}%`};
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 0;
+`;
+
+export const TableCell = styled(FlexDivCentered)<{ justifyContent?: string }>`
+    min-width: 0px;
+    z-index: 1;
+    color: var(--primary-color);
+    justify-content: ${(props) => props.justifyContent || 'left'};
+    &:first-child {
+        padding-left: 18px;
+    }
+    &:last-child {
+        padding-right: 18px;
+    }
+`;
+
+const NoResultContainer = styled(TableRow)`
+    min-height: 10px;
+    height: 60px;
+    padding-left: 18px;
+    font-size: 14px;
+    display: flex;
+    height: 100%;
+    overflow: hidden;
+`;
+
+export default OrderbookTable;
---
+++ src/pages/AMMTrading/components/OrderbookView/components/Orderbook/components/OrderbookTable/OrderbookTableHeader.tsx
@@ -0,0 +1,106 @@
+import { LightTooltip, StyledQuestionMarkIcon } from 'pages/Options/Market/components';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+import { TableRow, TableCell } from './OrderbookTable';
+
+type OrderbookTableHeaderProps = {
+    optionsCurrencyKey: string;
+};
+
+const OrderbookTableHeader: React.FC<OrderbookTableHeaderProps> = ({ optionsCurrencyKey }) => {
+    const { t } = useTranslation();
+    const columns = [
+        {
+            header: <></>,
+            width: 14,
+            justifyContent: 'left',
+        },
+        {
+            header: <p style={{ marginLeft: 14 }}>{t('options.market.trade-options.orderbook.table.price-col')}</p>,
+            width: 300,
+            justifyContent: 'left',
+        },
+        {
+            header: (
+                <>{t('options.market.trade-options.orderbook.table.amount-col', { currencyKey: optionsCurrencyKey })}</>
+            ),
+            width: 300,
+            justifyContent: 'left',
+        },
+        {
+            header: <>{t('options.market.trade-options.orderbook.table.total-col')}</>,
+            width: 300,
+            justifyContent: 'left',
+        },
+        {
+            header: (
+                <>
+                    {t('options.market.trade-options.orderbook.table.return-col')}
+                    <LightTooltip title={t('options.market.trade-options.orderbook.table.return-col-tooltip')}>
+                        <TableHeadQuestionMarkIcon />
+                    </LightTooltip>
+                </>
+            ),
+            className: 'return',
+            width: 300,
+            justifyContent: 'left',
+        },
+        {
+            header: <>{t('options.market.trade-options.orderbook.table.time-remaining-col')}</>,
+            width: 300,
+            justifyContent: 'center',
+            className: 'time-remaining',
+        },
+        {
+            header: <></>,
+            width: 50,
+            justifyContent: 'left',
+        },
+    ];
+
+    return (
+        <TableRow>
+            {columns.map((column: any, columnIndex: any) => (
+                <TableCellHead
+                    style={{ width: column.width }}
+                    key={columnIndex}
+                    justifyContent={column.justifyContent}
+                    className={column.className}
+                >
+                    {column.header}
+                </TableCellHead>
+            ))}
+        </TableRow>
+    );
+};
+
+const TableCellHead = styled(TableCell)<{ justifyContent?: string }>`
+    justify-content: ${(props) => props.justifyContent || 'left'};
+    font-weight: bold;
+    font-size: 10px;
+    line-height: 16px;
+    letter-spacing: 1px;
+    font-family: Roboto !important;
+    color: var(--table-header-text-color);
+    user-select: none;
+    &:first-child {
+        padding-left: 0;
+    }
+    @media (max-width: 512px) {
+        &.time-remaining {
+            text-align: center;
+        }
+        &.return svg {
+            display: none;
+        }
+    }
+`;
+
+const TableHeadQuestionMarkIcon = styled(StyledQuestionMarkIcon)`
+    path {
+        fill: var(--table-header-text-color);
+    }
+`;
+
+export default OrderbookTableHeader;
