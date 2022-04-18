bb34c789b85705f6d8cb4d66de41e57dd0090652
Gorshtak <sg31191@gmail.com>
2022-04-18 12:53:39 +0200

    fix for gelato query

--- src/queries/token/useGelatoQuery.ts
+++ src/queries/token/useGelatoQuery.ts
@@ -9,19 +9,19 @@ export interface Balance {
     apr: string;
 }
 
+const priceL2ThalesURL =
+    'https://api.1inch.exchange/v3.0/10/quote?fromTokenAddress=0x217D47011b23BB961eB6D93cA9945B7501a5BB11&toTokenAddress=0x7f5c764cbc14f9669b88837ca1490cca17c31607&amount=100000000000000000000';
+
 type Rates = { ethereum: { usd: number }; thales: { usd: number } };
 
 const getRates = async (): Promise<Rates> => {
-    const priceL2ThalesURL =
-        'https://api.1inch.exchange/v3.0/10/quote?fromTokenAddress=0x217D47011b23BB961eB6D93cA9945B7501a5BB11&toTokenAddress=0x7f5c764cbc14f9669b88837ca1490cca17c31607&amount=1000000000000000000';
-
     const [res1, res2] = await Promise.all([
         fetch(priceL2ThalesURL),
         fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd'),
     ]);
     const data1 = await res1.json();
     const data2 = await res2.json();
-    const toAmount = Number(ethers.utils.formatUnits(data1.toTokenAmount, data1.toToken.decimals)).toFixed(2);
+    const toAmount = (Number(ethers.utils.formatUnits(data1.toTokenAmount, data1.toToken.decimals)) / 100).toFixed(4);
 
     return {
         ...data2,
