43999d1dd16a3a055f369d2af837676ba62521ba
Gorshtak <sg31191@gmail.com>
2022-04-18 10:30:12 +0200

    fixed matomo

--- src/pages/Root/Root.tsx
+++ src/pages/Root/Root.tsx
@@ -9,6 +9,22 @@ import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react';
 const instance = createInstance({
     urlBase: 'https://matomo.thalesmarket.io',
     siteId: 2,
+    trackerUrl: '/js', // optional, default value: `${urlBase}matomo.php`
+    srcUrl: '/js', //
+    configurations: {
+        // optional, default value: {}
+        // any valid matomo configuration, all below are optional
+        disableCookies: true,
+        setSecureCookie: true,
+        setRequestMethod: 'POST',
+    },
+    disabled: false, // optional, false by default. Makes all tracking calls no-ops if set to true.
+    heartBeat: {
+        // optional, enabled by default
+        active: true, // optional, default value: true
+        seconds: 10, // optional, default value: `15
+    },
+    linkTracking: true, // optional, default value: true
 });
 
 interface RootProps {
