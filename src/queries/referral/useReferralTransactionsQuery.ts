8b0d8f5bf4c14ed2b4beabd596174c787ac9f5c9
makiBaraba <markovic.ofsec@gmail.com>
2022-05-28 18:26:24 +0200

    Referral Queries

---
+++ src/queries/referral/useReferralTransactionsQuery.ts
@@ -0,0 +1,36 @@
+import { useQuery, UseQueryOptions } from 'react-query';
+import QUERY_KEYS from 'constants/queryKeys';
+import { NetworkId } from 'utils/network';
+import thalesData from 'thales-data';
+
+type ReferralTransactions = {
+    id: string;
+    refferer: string;
+    trader: string;
+    amount: number;
+    volume: number;
+    timestamp: number;
+};
+
+const useReferralTransactionsQuery = (
+    networkId: NetworkId,
+    trader?: string,
+    refferer?: string,
+    options?: UseQueryOptions<ReferralTransactions[]>
+) => {
+    return useQuery<ReferralTransactions[]>(
+        QUERY_KEYS.Referral.ReferralTransacations(networkId, trader, refferer),
+        async () => {
+            const referrers: ReferralTransactions[] = await thalesData.binaryOptions.referralTransfers({
+                max: Infinity,
+                network: networkId,
+                trader,
+                refferer,
+            });
+            return referrers;
+        },
+        options
+    );
+};
+
+export default useReferralTransactionsQuery;
