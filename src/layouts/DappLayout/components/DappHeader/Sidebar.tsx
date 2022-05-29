9787dfef1ee036b30c9eba141e1b441739d2b19d
makiBaraba <markovic.ofsec@gmail.com>
2022-05-29 11:02:39 +0200

    Added to Sidebar

--- src/layouts/DappLayout/components/DappHeader/Sidebar.tsx
+++ src/layouts/DappLayout/components/DappHeader/Sidebar.tsx
@@ -95,6 +95,14 @@ const Sidebar: React.FC = () => {
                         label={t('common.sidebar.profile-label')}
                     />
                 )}
+                {!isPolygon && (
+                    <DappHeaderItem
+                        className={location.pathname === ROUTES.Options.Referral ? 'selected' : ''}
+                        href={buildHref(ROUTES.Options.Referral)}
+                        iconName="referral-page"
+                        label={t('referral-page.title')}
+                    />
+                )}
                 <Divider />
                 <DappHeaderItem
                     href={LINKS.ExoticMarkets}
