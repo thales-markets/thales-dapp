9d4b17d2633e0775a00067231f2ef35afe78e73b
Vojin Petrovic <vojinp13@gmail.com>
2022-06-03 16:28:29 +0200

    Fixed LP Staking TVL

--- src/queries/token/useLPStakingQuery.ts
+++ src/queries/token/useLPStakingQuery.ts
@@ -8,6 +8,7 @@ type LPStakingThalesQueryResponse = {
     staked: number;
     rewards: number;
     paused: boolean;
+    totalGelatoLocked: number;
 };
 
 const useLPStakingThalesQuery = (
@@ -35,6 +36,7 @@ const useLPStakingThalesQuery = (
                 staked: 0,
                 rewards: 0,
                 paused: false,
+                totalGelatoLocked: 0,
             };
 
             try {
@@ -46,16 +48,23 @@ const useLPStakingThalesQuery = (
                 //     Number(maxAMMVolumeRewardsPercentage) +
                 //     Number(maxThalesRoyaleRewardsPercentage);
 
+                const [paused, totalGelatoLocked] = await Promise.all([
+                    (snxJSConnector as any).lpStakingRewardsContract.paused(),
+                    (snxJSConnector as any).lpStakingRewardsContract.totalSupply(),
+                ]);
+
+                staking.paused = paused;
+                staking.totalGelatoLocked = bigNumberFormatter(totalGelatoLocked);
+
                 if (walletAddress !== '') {
-                    const [staked, rewards, paused] = await Promise.all([
+                    const [staked, rewards] = await Promise.all([
                         (snxJSConnector as any).lpStakingRewardsContract.balanceOf(walletAddress),
                         (snxJSConnector as any).lpStakingRewardsContract.earned(walletAddress),
-                        (snxJSConnector as any).lpStakingRewardsContract.paused(),
                     ]);
 
                     staking.staked = bigNumberFormatter(staked);
                     staking.rewards = bigNumberFormatter(rewards);
-                    staking.paused = paused;
+
                     // const [isUnstaking, lastUnstakeTime, thalesStaked, unstakingAmount, rewards] = await Promise.all([
                     //     (snxJSConnector as any).stakingThalesContract.unstaking(walletAddress),
                     //     (snxJSConnector as any).stakingThalesContract.lastUnstakeTime(walletAddress),
