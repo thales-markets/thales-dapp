81dbe92df8eaace954306be1e832e3c9d227ebf6
Gorshtak <sg31191@gmail.com>
2022-06-27 11:49:00 +0200

    removed banxa from UI

--- src/pages/Wizzard/Wizzard.tsx
+++ src/pages/Wizzard/Wizzard.tsx
@@ -1,10 +1,6 @@
-import Button from 'components/Button';
-import React, { useMemo, useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
 import Steps from './components/Steps';
 import WizzardText from './components/WizzardText';
-// import https from 'https';
-import * as crypto from 'crypto';
 
 enum WizzardSteps {
     None,
@@ -14,34 +10,21 @@ enum WizzardSteps {
     Trade,
 }
 
-enum OnRampProvider {
-    MtPelerin,
-    Banxa,
-}
+// enum OnRampProvider {
+//     MtPelerin,
+//     Banxa,
+// }
 
-const getPaymentMethods = '/api/coins/buy';
 const mtPelerinIframe =
     'https://widget.mtpelerin.com/?type=popup&lang=en&primary=%2304045a&mylogo=https://thalesmarket.io/THALES_LOGOTIP.svg&net=optimism_mainnet&bsc=EUR&bdc=ETH&crys=ETH';
-const banxaIframe = 'https://thalesmarket.banxa.com/iframe?code=x68QxHYZ2hQU0rccKDgDSeUO7QonDXsY?blockchain=XRP';
+// const banxaIframe = 'https://thalesmarket.banxa.com/iframe?code=x68QxHYZ2hQU0rccKDgDSeUO7QonDXsY?blockchain=XRP';
 
 const Wizzard: React.FC = () => {
     const [currentStep, setCurrentStep] = useState(WizzardSteps.None);
-    const [onRampProvider, setProvider] = useState(OnRampProvider.MtPelerin);
-
-    const iFrameUrl = useMemo(() => {
-        if (onRampProvider === OnRampProvider.MtPelerin) {
-            return mtPelerinIframe;
-        } else {
-            sendGetRequest(getPaymentMethods);
-            return banxaIframe;
-        }
-    }, [onRampProvider]);
-
-    console.log(iFrameUrl);
 
     return (
         <>
-            <ProviderWrapper>
+            {/* <ProviderWrapper>
                 <Button
                     padding={'5px 20px'}
                     onClickHandler={setProvider.bind(this, OnRampProvider.MtPelerin)}
@@ -56,8 +39,8 @@ const Wizzard: React.FC = () => {
                 >
                     Banxa
                 </Button>
-            </ProviderWrapper>
-            <Steps iframe={iFrameUrl} step={currentStep} setCurrentStep={setCurrentStep}></Steps>
+            </ProviderWrapper> */}
+            <Steps iframe={mtPelerinIframe} step={currentStep} setCurrentStep={setCurrentStep}></Steps>
             <WizzardText step={currentStep}></WizzardText>
         </>
     );
@@ -67,46 +50,46 @@ const Wizzard: React.FC = () => {
 // API Key: thalesmarket@2022test
 // Secret: x68QxHYZ2hQU0rccKDgDSeUO7QonDXsY
 
-function generateHmac(signature: any, nonce: any) {
-    const key = 'thalesmarket@2022test';
-    const secret = 'x68QxHYZ2hQU0rccKDgDSeUO7QonDXsY';
+// function generateHmac(signature: any, nonce: any) {
+//     const key = 'thalesmarket@2022test';
+//     const secret = 'x68QxHYZ2hQU0rccKDgDSeUO7QonDXsY';
 
-    const localSignature = crypto.createHmac('SHA256', secret).update(signature).digest('hex');
-    return `${key}:${localSignature}:${nonce}`;
-}
+//     const localSignature = crypto.createHmac('SHA256', secret).update(signature).digest('hex');
+//     return `${key}:${localSignature}:${nonce}`;
+// }
 
-function sendGetRequest(query: string) {
-    const hostname = 'thalesmarket.banxa-sandbox.com';
-    const nonce = Date.now();
-    const method = 'GET';
-    const data = method + '\n' + query + '\n' + nonce;
+// function sendGetRequest(query: string) {
+//     const hostname = 'thalesmarket.banxa-sandbox.com';
+//     const nonce = Date.now();
+//     const method = 'GET';
+//     const data = method + '\n' + query + '\n' + nonce;
 
-    const hmac = generateHmac(data, nonce);
-    const options = {
-        hostname: hostname,
-        path: query,
-        method: method,
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${hmac}`,
-        },
-    };
+//     const hmac = generateHmac(data, nonce);
+//     const options = {
+//         hostname: hostname,
+//         path: query,
+//         method: method,
+//         headers: {
+//             'Content-Type': 'application/json',
+//             Authorization: `Bearer ${hmac}`,
+//         },
+//     };
 
-    fetch('https://' + hostname + query, options).then(async (res: any) => {
-        const result = JSON.parse(await res.text());
-        console.log(result);
-        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    });
-}
+//     fetch('https://' + hostname + query, options).then(async (res: any) => {
+//         const result = JSON.parse(await res.text());
+//         console.log(result);
+//         console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+//     });
+// }
 
-const ProviderWrapper = styled.div`
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    gap: 12px;
-    width: 100%;
-    margin-top: -20px;
-    margin-bottom: 80px;
-`;
+// const ProviderWrapper = styled.div`
+//     display: flex;
+//     justify-content: flex-start;
+//     align-items: center;
+//     gap: 12px;
+//     width: 100%;
+//     margin-top: -20px;
+//     margin-bottom: 80px;
+// `;
 
 export default Wizzard;
