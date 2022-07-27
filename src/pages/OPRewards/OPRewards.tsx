2b8f55cf7127475fd50a134bb58735f53ab13cd8
dmikic <dusan.mikic15@gmail.com>
2022-07-27 12:54:27 +0200

    Added rewards info for user

--- src/pages/OPRewards/OPRewards.tsx
+++ src/pages/OPRewards/OPRewards.tsx
@@ -7,23 +7,26 @@ import SearchField from 'components/TableInputs/SearchField';
 
 import { useSelector } from 'react-redux';
 import { getIsAppReady } from 'redux/modules/app';
-import { getNetworkId } from 'redux/modules/wallet';
+import { getIsWalletConnected, getNetworkId, getWalletAddress } from 'redux/modules/wallet';
 import { RootState } from 'redux/rootReducer';
 import Loader from 'components/Loader';
 import { Trans, useTranslation } from 'react-i18next';
 import useUsersAmmBuyVolumeQuery from 'queries/user/useUsersAmmBuyVolumeQuery';
 import { truncateAddress } from 'utils/formatters/string';
 import Tooltip from 'components/Tooltip';
+import UserRewards from './components';
 
 const OPRewards: React.FC = () => {
     const networkId = useSelector((state: RootState) => getNetworkId(state));
     const isAppReady = useSelector((state: RootState) => getIsAppReady(state));
+    const walletAddress = useSelector((state: RootState) => getWalletAddress(state)) || '';
+    const isWalletConnected = useSelector((state: RootState) => getIsWalletConnected(state));
     const { t } = useTranslation();
 
     const [searchQuery, setSearchQuery] = useState<string>('');
 
-    const PERIOD_DURATION_IN_DAYS = 7;
-    const START_DATE = new Date(2022, 6, 13, 14, 30, 0);
+    const PERIOD_DURATION_IN_DAYS = 14;
+    const START_DATE = new Date(2022, 6, 13, 14, 23, 0);
     const NOW = new Date();
 
     let CALCULATED_START = new Date(START_DATE.getTime());
@@ -51,12 +54,14 @@ const OPRewards: React.FC = () => {
         }
     }
 
-    const [period, setPeriod] = useState<number>(options[options.length - 1].value);
+    const [period, setPeriod] = useState<number>(options.length > 0 ? options[options.length - 1].value : 0);
 
     const minTimestamp = periodRangeTimestamps[period]?.minTimestamp || undefined;
     const maxTimestamp = periodRangeTimestamps[period]?.maxTimestamp || undefined;
 
     const usersAmmBuyVolumeQuery = useUsersAmmBuyVolumeQuery(networkId, period, { enabled: isAppReady });
+
+    const [userData, setUserData] = useState<Array<any>>([]);
 
     const tableData = useMemo(() => {
         if (usersAmmBuyVolumeQuery?.data && usersAmmBuyVolumeQuery?.isSuccess) {
@@ -72,6 +77,19 @@ const OPRewards: React.FC = () => {
             }> = [];
 
             transactions.rewards.forEach((reward) => {
+                if (walletAddress === reward.address) {
+                    setUserData([
+                        {
+                            account: reward.address,
+                            upInfo: reward.upInfo,
+                            downInfo: reward.downInfo,
+                            rangedInfo: reward.rangedInfo,
+                            calculatedProtocolBonusForPeriod: reward.staking.toFixed(2),
+                            totalRewards: reward.totalRewards,
+                        },
+                    ]);
+                }
+
                 data.push({
                     account: reward.address,
                     upInfo: reward.upInfo,
@@ -120,126 +138,129 @@ const OPRewards: React.FC = () => {
             {isLoading ? (
                 <Loader />
             ) : (
-                <Table
-                    containerStyle={{
-                        width: '100%',
-                        maxWidth: '100%',
-                    }}
-                    data={tableData}
-                    columns={[
-                        {
-                            Header: t('op-rewards.table.wallet-address'),
-                            accessor: 'account',
-                            Cell: (cellProps: any) => (
-                                <p style={{ width: '100%', textAlign: 'center' }}>
-                                    {truncateAddress(cellProps.cell.value)}
-                                </p>
-                            ),
-                        },
-                        {
-                            Header: t('op-rewards.table.up-info'),
-                            accessor: 'upInfo',
-                            Cell: (cellProps: any) => (
-                                <p style={{ width: '100%', textAlign: 'center' }}>
-                                    <Trans
-                                        i18nKey={'op-rewards.table.reward-text'}
-                                        values={{
-                                            thales: Number(cellProps.cell.value.rewards.thales).toFixed(2),
-                                            op: Number(cellProps.cell.value.rewards.op).toFixed(2),
-                                        }}
-                                        components={[<br key="0" />]}
-                                    />
-                                    <Tooltip
-                                        message={t('op-rewards.table.info-text', {
-                                            volume: Number(cellProps.cell.value.volume).toFixed(2),
-                                            percentage: (Number(cellProps.cell.value.percentage) * 100).toFixed(2),
-                                        })}
-                                        type={'info'}
-                                        iconColor={'var(--primary-color)'}
-                                        container={{ display: 'inline-block' }}
-                                        interactive={true}
-                                    />
-                                </p>
-                            ),
-                            disableSortBy: true,
-                        },
-                        {
-                            Header: t('op-rewards.table.down-info'),
-                            accessor: 'downInfo',
-                            Cell: (cellProps: any) => (
-                                <p style={{ width: '100%', textAlign: 'center' }}>
-                                    <Trans
-                                        i18nKey={'op-rewards.table.reward-text'}
-                                        values={{
-                                            thales: Number(cellProps.cell.value.rewards.thales).toFixed(2),
-                                            op: Number(cellProps.cell.value.rewards.op).toFixed(2),
-                                        }}
-                                        components={[<br key="0" />]}
-                                    />
-                                    <Tooltip
-                                        message={t('op-rewards.table.info-text', {
-                                            volume: Number(cellProps.cell.value.volume).toFixed(2),
-                                            percentage: (Number(cellProps.cell.value.percentage) * 100).toFixed(2),
-                                        })}
-                                        type={'info'}
-                                        iconColor={'var(--primary-color)'}
-                                        container={{ display: 'inline-block' }}
-                                        interactive={true}
-                                    />
-                                </p>
-                            ),
-                            disableSortBy: true,
-                        },
-                        {
-                            Header: t('op-rewards.table.ranged-info'),
-                            accessor: 'rangedInfo',
-                            Cell: (cellProps: any) => (
-                                <p style={{ width: '100%', textAlign: 'center' }}>
-                                    <Trans
-                                        i18nKey={'op-rewards.table.reward-text'}
-                                        values={{
-                                            thales: Number(cellProps.cell.value.rewards.thales).toFixed(2),
-                                            op: Number(cellProps.cell.value.rewards.op).toFixed(2),
-                                        }}
-                                        components={[<br key="0" />]}
-                                    />
-                                    <Tooltip
-                                        message={t('op-rewards.table.info-text', {
-                                            volume: Number(cellProps.cell.value.volume).toFixed(2),
-                                            percentage: (Number(cellProps.cell.value.percentage) * 100).toFixed(2),
-                                        })}
-                                        type={'info'}
-                                        iconColor={'var(--primary-color)'}
-                                        container={{ display: 'inline-block' }}
-                                        interactive={true}
-                                    />
-                                </p>
-                            ),
-                            disableSortBy: true,
-                        },
-                        {
-                            Header: t('op-rewards.table.protocol-reward'),
-                            accessor: 'calculatedProtocolBonusForPeriod',
-                            Cell: (cellProps: any) => <p>{cellProps.cell.value} OP</p>,
-                        },
-                        {
-                            Header: t('op-rewards.table.total-rewards'),
-                            accessor: 'totalRewards',
-                            Cell: (cellProps: any) => (
-                                <p style={{ width: '100%', textAlign: 'center' }}>
-                                    <Trans
-                                        i18nKey={'op-rewards.table.total-text'}
-                                        values={{
-                                            thales: Number(cellProps.cell.value.thales).toFixed(2),
-                                            op: Number(cellProps.cell.value.op).toFixed(2),
-                                        }}
-                                        components={[<br key="0" />]}
-                                    />
-                                </p>
-                            ),
-                        },
-                    ]}
-                />
+                <>
+                    {isWalletConnected && <UserRewards userRewards={userData} />}
+                    <Table
+                        containerStyle={{
+                            width: '100%',
+                            maxWidth: '100%',
+                        }}
+                        data={tableData}
+                        columns={[
+                            {
+                                Header: t('op-rewards.table.wallet-address'),
+                                accessor: 'account',
+                                Cell: (cellProps: any) => (
+                                    <p style={{ width: '100%', textAlign: 'center' }}>
+                                        {truncateAddress(cellProps.cell.value)}
+                                    </p>
+                                ),
+                            },
+                            {
+                                Header: t('op-rewards.table.up-info'),
+                                accessor: 'upInfo',
+                                Cell: (cellProps: any) => (
+                                    <p style={{ width: '100%', textAlign: 'center' }}>
+                                        <Trans
+                                            i18nKey={'op-rewards.table.reward-text'}
+                                            values={{
+                                                thales: Number(cellProps.cell.value.rewards.thales).toFixed(2),
+                                                op: Number(cellProps.cell.value.rewards.op).toFixed(2),
+                                            }}
+                                            components={[<br key="0" />]}
+                                        />
+                                        <Tooltip
+                                            message={t('op-rewards.table.info-text', {
+                                                volume: Number(cellProps.cell.value.volume).toFixed(2),
+                                                percentage: (Number(cellProps.cell.value.percentage) * 100).toFixed(2),
+                                            })}
+                                            type={'info'}
+                                            iconColor={'var(--primary-color)'}
+                                            container={{ display: 'inline-block' }}
+                                            interactive={true}
+                                        />
+                                    </p>
+                                ),
+                                disableSortBy: true,
+                            },
+                            {
+                                Header: t('op-rewards.table.down-info'),
+                                accessor: 'downInfo',
+                                Cell: (cellProps: any) => (
+                                    <p style={{ width: '100%', textAlign: 'center' }}>
+                                        <Trans
+                                            i18nKey={'op-rewards.table.reward-text'}
+                                            values={{
+                                                thales: Number(cellProps.cell.value.rewards.thales).toFixed(2),
+                                                op: Number(cellProps.cell.value.rewards.op).toFixed(2),
+                                            }}
+                                            components={[<br key="0" />]}
+                                        />
+                                        <Tooltip
+                                            message={t('op-rewards.table.info-text', {
+                                                volume: Number(cellProps.cell.value.volume).toFixed(2),
+                                                percentage: (Number(cellProps.cell.value.percentage) * 100).toFixed(2),
+                                            })}
+                                            type={'info'}
+                                            iconColor={'var(--primary-color)'}
+                                            container={{ display: 'inline-block' }}
+                                            interactive={true}
+                                        />
+                                    </p>
+                                ),
+                                disableSortBy: true,
+                            },
+                            {
+                                Header: t('op-rewards.table.ranged-info'),
+                                accessor: 'rangedInfo',
+                                Cell: (cellProps: any) => (
+                                    <p style={{ width: '100%', textAlign: 'center' }}>
+                                        <Trans
+                                            i18nKey={'op-rewards.table.reward-text'}
+                                            values={{
+                                                thales: Number(cellProps.cell.value.rewards.thales).toFixed(2),
+                                                op: Number(cellProps.cell.value.rewards.op).toFixed(2),
+                                            }}
+                                            components={[<br key="0" />]}
+                                        />
+                                        <Tooltip
+                                            message={t('op-rewards.table.info-text', {
+                                                volume: Number(cellProps.cell.value.volume).toFixed(2),
+                                                percentage: (Number(cellProps.cell.value.percentage) * 100).toFixed(2),
+                                            })}
+                                            type={'info'}
+                                            iconColor={'var(--primary-color)'}
+                                            container={{ display: 'inline-block' }}
+                                            interactive={true}
+                                        />
+                                    </p>
+                                ),
+                                disableSortBy: true,
+                            },
+                            {
+                                Header: t('op-rewards.table.protocol-reward'),
+                                accessor: 'calculatedProtocolBonusForPeriod',
+                                Cell: (cellProps: any) => <p>{cellProps.cell.value} OP</p>,
+                            },
+                            {
+                                Header: t('op-rewards.table.total-rewards'),
+                                accessor: 'totalRewards',
+                                Cell: (cellProps: any) => (
+                                    <p style={{ width: '100%', textAlign: 'center' }}>
+                                        <Trans
+                                            i18nKey={'op-rewards.table.total-text'}
+                                            values={{
+                                                thales: Number(cellProps.cell.value.thales).toFixed(2),
+                                                op: Number(cellProps.cell.value.op).toFixed(2),
+                                            }}
+                                            components={[<br key="0" />]}
+                                        />
+                                    </p>
+                                ),
+                            },
+                        ]}
+                    />
+                </>
             )}
         </Wrapper>
     );
