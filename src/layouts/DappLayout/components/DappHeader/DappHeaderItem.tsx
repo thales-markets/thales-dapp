5b6b99f6bdf0556e864a404e35f10c7f6fd9f6b4
Vladan Poklopic <vladanpoklopic@gmail.com>
2022-08-01 10:05:03 +0200

    Pull overtime icon from sidebar font file

--- src/layouts/DappLayout/components/DappHeader/DappHeaderItem.tsx
+++ src/layouts/DappLayout/components/DappHeader/DappHeaderItem.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import SPAAnchor from 'components/SPAAnchor';
 import styled from 'styled-components';
-import overtimeIcon from 'assets/images/sidebar/overtime.svg';
 
 type MenuItem = {
     className?: string;
@@ -50,14 +49,10 @@ const DappHeaderItem: React.FC<DappHeaderItemProps> = ({
     return (
         <SPAAnchor href={href || ''} onClick={onClick} simpleOnClick={simpleOnClick}>
             <MenuItem className={className}>
-                {iconName === 'overtime-markets' ? (
-                    <Icon src={overtimeIcon} />
-                ) : (
-                    <i
-                        className={`sidebar-icon icon--${iconName}`}
-                        style={iconName == 'optimism' ? { color: 'white' } : {}}
-                    />
-                )}
+                <i
+                    className={`sidebar-icon icon--${iconName}`}
+                    style={iconName == 'optimism' ? { color: 'white' } : {}}
+                />
                 <Text>{label}</Text>
             </MenuItem>
         </SPAAnchor>
@@ -125,11 +120,6 @@ const Arrow = styled.i<{ open: boolean }>`
     right: 10px;
     transform: rotate(${(props) => (props.open ? 90 : -90)}deg);
     font-size: 14px;
-`;
-
-const Icon = styled.img`
-    min-width: 32px;
-    display: block;
 `;
 
 export default DappHeaderItem;
