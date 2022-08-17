43e4c32235e28e83394052204ea9d5483e9a6135
makiBaraba <markovic.ofsec@gmail.com>
2022-08-17 11:09:06 +0200

    Shorter version

--- src/pages/SalesLanding/SalesLanding.tsx
+++ src/pages/SalesLanding/SalesLanding.tsx
@@ -2,6 +2,8 @@ import { AnimationSvg } from 'pages/LandingPage/articles/Token/Token';
 import Header from 'pages/LandingPage/components/Header/Header';
 import { Theme } from 'pages/LandingPage/Home';
 import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
 import { useTranslation } from 'react-i18next';
 import { HashLink } from 'react-router-hash-link';
 import Cookies from 'universal-cookie';
@@ -36,8 +38,10 @@ const cookies = new Cookies();
 const SalesLanding: React.FC = () => {
     const { t } = useTranslation();
 
-    const salesFunnelType = 0;
-
+    const rawParams = useLocation();
+    const queryParams = queryString.parse(rawParams?.search);
+    const salesFunnelType = queryParams?.type || 0;
+    console.log('queryParams ', queryParams);
     const [theme, setTheme] = useState(Number(cookies.get('home-theme')) === 0 ? Theme.Light : Theme.Dark);
     return (
         <Background className={theme === Theme.Light ? 'light' : 'dark'}>
@@ -204,6 +208,93 @@ const SalesLanding: React.FC = () => {
                                 <ParagraphContainer width="100%">
                                     <H2 id="section10">{t('sales-landing.sections.10.heading')}</H2>
                                     <Paragraph>{t('sales-landing.sections.10.content')}</Paragraph>
+                                </ParagraphContainer>
+                            </SectionWrapper>
+                        </>
+                    )}
+                    {salesFunnelType == 1 && (
+                        <>
+                            <H1>{t('sales-landing.title')}</H1>
+                            <SectionWrapper>
+                                <ParagraphContainer width="30%">
+                                    <H2>{t('sales-landing.sections-short-version.1.heading')}</H2>
+                                    <Paragraph>{t('sales-landing.sections-short-version.1.content')}</Paragraph>
+                                </ParagraphContainer>
+                                <IllustrationContainer width="70%">
+                                    <iframe
+                                        width="100%"
+                                        height="480"
+                                        src="https://www.youtube.com/embed/e8EKlUeQGLA"
+                                        frameBorder="0"
+                                        allowFullScreen
+                                    ></iframe>
+                                </IllustrationContainer>
+                            </SectionWrapper>
+                            <SectionWrapper>
+                                <IllustrationContainer width="40%" style={{ marginRight: '30px' }}>
+                                    <AnimationSvg type="image/svg+xml" data={UPMarket}></AnimationSvg>
+                                </IllustrationContainer>
+                                <ParagraphContainer width="60%">
+                                    <H2>{t('sales-landing.sections-short-version.2.heading')}</H2>
+                                    <Paragraph>{t('sales-landing.sections.2.content')}</Paragraph>
+                                </ParagraphContainer>
+                            </SectionWrapper>
+                            <SectionWrapper>
+                                <IllustrationContainer width="40%" style={{ marginRight: '30px' }}>
+                                    <AnimationSvg type="image/svg+xml" data={DOWNMarket}></AnimationSvg>
+                                </IllustrationContainer>
+                                <ParagraphContainer width="60%">
+                                    <H2>{t('sales-landing.sections-short-version.3.heading')}</H2>
+                                    <Paragraph>{t('sales-landing.sections-short-version.3.content')}</Paragraph>
+                                </ParagraphContainer>
+                            </SectionWrapper>
+                            <SectionWrapper>
+                                <IllustrationContainer width="40%" style={{ marginRight: '30px' }}>
+                                    <AnimationSvg type="image/svg+xml" data={INMarket}></AnimationSvg>
+                                </IllustrationContainer>
+                                <ParagraphContainer width="60%">
+                                    <H2>{t('sales-landing.sections-short-version.4.heading')}</H2>
+                                    <Paragraph>{t('sales-landing.sections-short-version.4.content')}</Paragraph>
+                                </ParagraphContainer>
+                            </SectionWrapper>
+                            <SectionWrapper>
+                                <IllustrationContainer width="40%" style={{ marginRight: '30px' }}>
+                                    <AnimationSvg type="image/svg+xml" data={OUTMarket}></AnimationSvg>
+                                </IllustrationContainer>
+                                <ParagraphContainer width="60%">
+                                    <H2>{t('sales-landing.sections-short-version.5.heading')}</H2>
+                                    <Paragraph>{t('sales-landing.sections-short-version.5.content')}</Paragraph>
+                                </ParagraphContainer>
+                            </SectionWrapper>
+                            <SectionWrapper
+                                style={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'space-between',
+                                    marginTop: '50px',
+                                }}
+                            >
+                                <IllustrationContainer width="40%">
+                                    <AnimationSvg type="image/svg+xml" data={DOWNChart}></AnimationSvg>
+                                </IllustrationContainer>
+                                <IllustrationContainer width="40%">
+                                    <AnimationSvg type="image/svg+xml" data={UPChart}></AnimationSvg>
+                                </IllustrationContainer>
+                            </SectionWrapper>
+                            <SectionWrapper
+                                style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}
+                            >
+                                <IllustrationContainer width="40%">
+                                    <AnimationSvg type="image/svg+xml" data={INChart}></AnimationSvg>
+                                </IllustrationContainer>
+                                <IllustrationContainer width="40%">
+                                    <AnimationSvg type="image/svg+xml" data={OUTChart}></AnimationSvg>
+                                </IllustrationContainer>
+                            </SectionWrapper>
+                            <SectionWrapper>
+                                <ParagraphContainer width="100%">
+                                    <H2>{t('sales-landing.sections-short-version.6.heading')}</H2>
+                                    <Paragraph>{t('sales-landing.sections-short-version.6.content')}</Paragraph>
                                 </ParagraphContainer>
                             </SectionWrapper>
                         </>
