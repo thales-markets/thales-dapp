0913c9cf16032ccda0477208df040c474225d439
makiBaraba <markovic.ofsec@gmail.com>
2022-09-02 13:10:35 +0200

    AMM new contract address

--- src/utils/contracts/ammContract.ts
+++ src/utils/contracts/ammContract.ts
@@ -12,7 +12,7 @@ export const ammContract = {
         [Network['Kovan-Ovm']]: '0xfed727f37e921Faac22e36E0bbaA8504B45F6e3f',
         [Network['POLYGON-MUMBAI']]: '0xdf0A5bf80E8009f2702E7d164A7bdeaa79013eba',
         [Network['POLYGON-MAINNET']]: '0x9b6d76B1C6140FbB0ABc9C4a348BFf4e4e8a1213',
-        [Network.BSC]: '0x85f1B57A1D3Ac7605de3Df8AdA056b3dB9676eCE',
+        [Network.BSC]: '0x465B66A3e33088F0666dB1836652fBcF037c7319',
     },
     abi: [
         {
