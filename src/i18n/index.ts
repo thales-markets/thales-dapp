2e0d7d689d38d29e7e543dc3688c38db7cac591e
Vladan Poklopic <vladanpoklopic@gmail.com>
2022-11-25 16:01:24 +0100

    Dummy commit

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import { DEFAULT_LANGUAGE, resources } from './config';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n.use(LanguageDetector)
     .use(initReactI18next)
