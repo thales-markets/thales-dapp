f26ed417982d58a200df16b91023bb10392aa8ed
vojin5rovic <vojinp13@gmail.com>
2022-12-28 14:56:52 +0100

    Tale of Thales - nft minting page design

--- src/pages/TaleOfThales/components/Mint/Mint.tsx
+++ src/pages/TaleOfThales/components/Mint/Mint.tsx
@@ -1,36 +1,88 @@
-import Button from 'components/Button';
 import React from 'react';
 import styled from 'styled-components';
-import { NFT_LIST } from './constants';
+import { NFT_COLLECTIONS } from './constants';
+import { ReactComponent as NFTContainerImage } from 'assets/images/tale-of-thales-nft-container.svg';
+import { ReactComponent as PixelTooltipContainer } from 'assets/images/pixel-tooltip-container.svg';
+import { Tooltip } from '@material-ui/core';
+import { ReactComponent as InfoIcon } from 'assets/images/info.svg';
 
 const Mint: React.FC = () => {
-    const defaultButtonProps = {
-        padding: '3px 35px',
-        active: true,
-        margin: '15px auto 0 auto',
-        hoverShadow: 'var(--button-shadow)',
-        fontSize: '16px',
-    };
     return (
-        <Grid>
-            {NFT_LIST.map((nft, index) => {
+        <Container>
+            {NFT_COLLECTIONS.map((collection, index) => {
                 return (
-                    <NFTContainer key={index}>
-                        <NFTImage src={nft.src} />
-                        <Button {...defaultButtonProps}>Mint</Button>
-                    </NFTContainer>
+                    <CollectionContainer key={index}>
+                        <CollectionTitleContainer>
+                            <CollectionTitle>{collection.name}</CollectionTitle>
+                            <StyledTooltip
+                                placement="right"
+                                title={
+                                    <>
+                                        <StyledPixelTooltipContainer />
+                                        <TooltipContent>{collection.condition}</TooltipContent>
+                                    </>
+                                }
+                            >
+                                <StyledInfoIcon />
+                            </StyledTooltip>
+                        </CollectionTitleContainer>
+                        <CollectionItemsContainer>
+                            {collection.items.map((item, index) => {
+                                return (
+                                    <Item key={index}>
+                                        <NFTContainer>
+                                            <StyledNFTContainerImage />
+                                            <NFTTitle>{item.name}</NFTTitle>
+                                            <NFTImage src={item.src} />
+                                        </NFTContainer>
+                                        <Button>Mint</Button>
+                                    </Item>
+                                );
+                            })}
+                            <CollectionButton>Mint Collection</CollectionButton>
+                        </CollectionItemsContainer>
+                    </CollectionContainer>
                 );
             })}
-        </Grid>
+        </Container>
     );
 };
 
-const Grid = styled.div`
-    display: grid;
+const Container = styled.div`
     width: 100%;
-    grid-template-columns: auto auto auto auto;
     padding: 40px;
-    row-gap: 40px;
+`;
+
+const CollectionContainer = styled.div`
+    margin-bottom: 40px;
+`;
+
+const CollectionItemsContainer = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const CollectionTitleContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+`;
+
+const CollectionTitle = styled.span`
+    font-family: basis33 !important;
+    color: var(--primary-color);
+    text-transform: uppercase;
+    font-size: 28px;
+    line-height: 105%;
+    letter-spacing: 2px;
+`;
+
+const NFTTitle = styled.span`
+    font-family: basis33 !important;
+    color: var(--background);
+    z-index: 1;
+    text-transform: uppercase;
+    font-size: 25px;
 `;
 
 const NFTContainer = styled.div`
@@ -38,10 +90,79 @@ const NFTContainer = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    width: 187px;
+    height: 166px;
+`;
+
+const Item = styled.div`
+    margin-right: 13px;
 `;
 
 const NFTImage = styled.img`
-    width: 100px;
+    width: 80px;
+    z-index: 1;
+`;
+
+const StyledNFTContainerImage = styled(NFTContainerImage)`
+    position: absolute;
+    z-index: 0;
+`;
+
+const Button = styled.button`
+    font-family: basis33 !important;
+    background: linear-gradient(90.42deg, #d6d0ab 18.36%, #aea992 87.84%);
+    border: 2px dashed #04045a;
+    color: var(--background);
+    width: 100%;
+    height: 35px;
+    font-size: 25px;
+    text-transform: uppercase;
+    margin-top: 10px;
+    cursor: pointer;
+`;
+
+const CollectionButton = styled.button`
+    font-family: basis33 !important;
+    background: linear-gradient(90.42deg, #d6d0ab 18.36%, #aea992 87.84%);
+    border: 2px dashed #04045a;
+    color: var(--background);
+    width: 350px;
+    height: 35px;
+    font-size: 25px;
+    text-transform: uppercase;
+    cursor: pointer;
+    transform: translateY(-50%);
+    margin-left: auto;
+`;
+
+const StyledTooltip = styled(({ className, ...props }) => <Tooltip {...props} classes={{ popper: className }} />)(
+    () => ({
+        [`& .MuiTooltip-tooltip`]: {
+            backgroundColor: 'transparent',
+            color: 'white',
+            maxWidth: 332,
+        },
+    })
+);
+
+const StyledPixelTooltipContainer = styled(PixelTooltipContainer)`
+    position: absolute;
+    top: -67px;
+    z-index: -1;
+`;
+
+const TooltipContent = styled.div`
+    font-family: basis33 !important;
+    font-size: 25px;
+    line-height: 120%;
+    text-align: justify;
+    width: 332px;
+    height: 167px;
+    padding: 10px 10px 10px 15px;
+`;
+
+const StyledInfoIcon = styled(InfoIcon)`
+    margin: 0 0 5px 5px;
 `;
 
 export default Mint;
--- src/pages/TaleOfThales/components/Mint/constants.ts
+++ src/pages/TaleOfThales/components/Mint/constants.ts
@@ -1,3 +1,81 @@
+export const NFT_COLLECTIONS = [
+    {
+        name: 'Miletus Soldier Collection',
+        condition:
+            '“Miletus Soldier Collection” is unlocked for all STAKERS who have staked more than X THALES before Y date',
+        items: [
+            { name: 'Helmet', src: '/miletus-nfts/NFT_0.png' },
+            { name: 'Breastplate', src: '/miletus-nfts/NFT_1.png' },
+            { name: 'Greaves', src: '/miletus-nfts/NFT_2.png' },
+            { name: 'Sword', src: '/miletus-nfts/NFT_3.png' },
+        ],
+    },
+    {
+        name: 'Bronze Soldier Collection',
+        condition:
+            '“Miletus Soldier Collection” is unlocked for all STAKERS who have staked more than X THALES before Y date',
+        items: [
+            { name: 'Helmet', src: '/miletus-nfts/NFT_4.png' },
+            { name: 'Breastplate', src: '/miletus-nfts/NFT_5.png' },
+            { name: 'Greaves', src: '/miletus-nfts/NFT_6.png' },
+            { name: 'Sword', src: '/miletus-nfts/NFT_7.png' },
+        ],
+    },
+    {
+        name: 'Silver Soldier Collection',
+        condition:
+            '“Miletus Soldier Collection” is unlocked for all STAKERS who have staked more than X THALES before Y date',
+        items: [
+            { name: 'Helmet', src: '/miletus-nfts/NFT_8.png' },
+            { name: 'Breastplate', src: '/miletus-nfts/NFT_9.png' },
+            { name: 'Greaves', src: '/miletus-nfts/NFT_10.png' },
+            { name: 'Sword', src: '/miletus-nfts/NFT_11.png' },
+        ],
+    },
+    {
+        name: 'Gold Soldier Collection',
+        condition:
+            '“Miletus Soldier Collection” is unlocked for all STAKERS who have staked more than X THALES before Y date',
+        items: [
+            { name: 'Helmet', src: '/miletus-nfts/NFT_12.png' },
+            { name: 'Breastplate', src: '/miletus-nfts/NFT_13.png' },
+            { name: 'Greaves', src: '/miletus-nfts/NFT_14.png' },
+            { name: 'Sword', src: '/miletus-nfts/NFT_15.png' },
+        ],
+    },
+    {
+        name: 'Fisherman Collection',
+        condition:
+            '“Miletus Soldier Collection” is unlocked for all STAKERS who have staked more than X THALES before Y date',
+        items: [
+            { name: 'Rod', src: '/miletus-nfts/NFT_16.png' },
+            { name: 'Hat', src: '/miletus-nfts/NFT_17.png' },
+            { name: 'Robe', src: '/miletus-nfts/NFT_18.png' },
+            { name: 'Boots', src: '/miletus-nfts/NFT_19.png' },
+        ],
+    },
+    {
+        name: 'Camouflage Fisherman Collection',
+        condition:
+            '“Miletus Soldier Collection” is unlocked for all STAKERS who have staked more than X THALES before Y date',
+        items: [
+            { name: 'Hat', src: '/miletus-nfts/NFT_20.png' },
+            { name: 'Robe', src: '/miletus-nfts/NFT_21.png' },
+            { name: 'Boots', src: '/miletus-nfts/NFT_22.png' },
+        ],
+    },
+    {
+        name: 'Luxury Fisherman Collection',
+        condition:
+            '“Miletus Soldier Collection” is unlocked for all STAKERS who have staked more than X THALES before Y date',
+        items: [
+            { name: 'Hat', src: '/miletus-nfts/NFT_23.png' },
+            { name: 'Robe', src: '/miletus-nfts/NFT_24.png' },
+            { name: 'Boots', src: '/miletus-nfts/NFT_25.png' },
+        ],
+    },
+];
+
 export const NFT_LIST = [
     {
         src: '/miletus-nfts/NFT_0.png',
