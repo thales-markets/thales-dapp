9b1db8f63c513b3bd7a1bf3383e418f5ea4706d8
Gorshtak <sg31191@gmail.com>
2023-04-04 17:14:35 +0200

    removed vaults from arbitrum for the release

--- src/constants/vault.ts
+++ src/constants/vault.ts
@@ -16,7 +16,7 @@ export const VAULT_MAP: Record<string, any> = {
             10: '0xb484027CB0c538538Bad2bE492714154f9196F93',
             42: '',
             420: '',
-            42161: '0x0A29CddbdAAf56342507574820864dAc967D2683',
+            // 42161: '0x0A29CddbdAAf56342507574820864dAc967D2683',
         },
     },
     'degen-discount-vault': {
@@ -25,7 +25,7 @@ export const VAULT_MAP: Record<string, any> = {
             10: '0x43318DE9E8f65b591598F17aDD87ae7247649C83',
             42: '',
             420: '',
-            42161: '0x640c34D9595AD5351Da8c5C833Bbd1AfD20519ea',
+            // 42161: '0x640c34D9595AD5351Da8c5C833Bbd1AfD20519ea',
         },
     },
     'safu-discount-vault': {
@@ -34,7 +34,7 @@ export const VAULT_MAP: Record<string, any> = {
             10: '0x6c7Fd4321183b542E81Bcc7dE4DfB88F9DBca29F',
             42: '',
             420: '',
-            42161: '0x008A4e30A8b41781F5cb017b197aA9Aa4Cd53b46',
+            // 42161: '0x008A4e30A8b41781F5cb017b197aA9Aa4Cd53b46',
         },
     },
 };
--- src/layouts/DappLayout/components/DappHeader/Sidebar.tsx
+++ src/layouts/DappLayout/components/DappHeader/Sidebar.tsx
@@ -32,7 +32,7 @@ const Sidebar: React.FC = () => {
     const showWizardPage = !isPolygon && !isMobileState && !isBSC && !isArbitrum;
     const showTokenPage = !isPolygon && !isBSC;
     const showOPRewardsPage = !isPolygon && !isBSC && !isArbitrum;
-    const showVaultsPage = !isPolygon && !isBSC;
+    const showVaultsPage = !isPolygon && !isBSC && !isArbitrum;
 
     useEffect(() => {
         const handleResize = debounce(() => {
