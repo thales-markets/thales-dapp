3a3400ecfe8717e3e873896d94b8b72bf497f9bf
makiBaraba <markovic.ofsec@gmail.com>
2023-07-10 10:20:36 +0200

    Highlight if connected wallet address

--- src/pages/Token/GamifiedStaking/StakingLeaderboard/components/HighlightCard/HighlightCard.tsx
+++ src/pages/Token/GamifiedStaking/StakingLeaderboard/components/HighlightCard/HighlightCard.tsx
@@ -6,51 +6,62 @@ import BronzeBadge from 'assets/images/token/leaderboard/bronze_badge.svg';
 import SilverBadge from 'assets/images/token/leaderboard/silver_badge.svg';
 import { truncateAddress } from 'utils/formatters/string';
 import { useTranslation } from 'react-i18next';
+import { getWalletAddress } from 'redux/modules/wallet';
+import { useSelector } from 'react-redux';
+import { RootState } from 'redux/rootReducer';
 
 type HighlightCardProps = {
     rank: number;
     walletAddress: string;
     totalPoints: string;
-    totalRewards: string;
+    totalRewards: string | undefined;
 };
 
 const HighlightCard: React.FC<HighlightCardProps> = ({ rank, walletAddress, totalPoints, totalRewards }) => {
     const { t } = useTranslation();
     const badge = rank == 1 ? GoldBadge : rank == 2 ? SilverBadge : rank == 3 ? BronzeBadge : '';
+    const connectedWalletAddress = useSelector((state: RootState) => getWalletAddress(state));
+
+    const highlightCard = walletAddress.toLowerCase() == connectedWalletAddress?.toLowerCase();
 
     return (
-        <Container>
+        <Container highlightCard={highlightCard}>
             <BadgeContainer>
                 <Badge src={badge} />
             </BadgeContainer>
-            <WalletAddress>{truncateAddress(walletAddress)}</WalletAddress>
+            <WalletAddress highlightCard={highlightCard}>{truncateAddress(walletAddress)}</WalletAddress>
             <RewardsDataContainer>
                 <DataContainer>
-                    <Heading>{t('profile.leaderboard.highlight-card.total-points')}</Heading>
-                    <Amount>{totalPoints}</Amount>
+                    <Heading highlightCard={highlightCard}>
+                        {t('profile.leaderboard.highlight-card.total-points')}
+                    </Heading>
+                    <Amount highlightCard={highlightCard}>{totalPoints}</Amount>
                 </DataContainer>
                 <DataContainer>
-                    <Heading>{t('profile.leaderboard.highlight-card.total-rewards')}</Heading>
-                    <Amount>{totalRewards}</Amount>
+                    <Heading highlightCard={highlightCard}>
+                        {t('profile.leaderboard.highlight-card.total-rewards')}
+                    </Heading>
+                    <Amount highlightCard={highlightCard}>{totalRewards ? totalRewards : ''}</Amount>
                 </DataContainer>
             </RewardsDataContainer>
         </Container>
     );
 };
 
-const Container = styled(FlexDiv)`
+const Container = styled(FlexDiv)<{ highlightCard: boolean }>`
     flex-direction: column;
     align-items: center;
     width: 350px;
     height: 167px;
+    background-color: ${(_props) => `${_props.highlightCard ? _props.theme.borderColor.tertiary : ''}`};
     border: ${(_props) => `1px solid ${_props.theme.borderColor.tertiary}`};
     border-radius: 8px;
 `;
 
-const WalletAddress = styled.span`
+const WalletAddress = styled.span<{ highlightCard: boolean }>`
     font-weight: 700;
     font-size: 18px;
-    color: ${(_props) => _props.theme.textColor.primary};
+    color: ${(_props) => `${!_props.highlightCard ? _props.theme.textColor.primary : _props.theme.background.primary}`};
     margin: 50px 0px 10px 0px;
 `;
 
@@ -62,16 +73,17 @@ const RewardsDataContainer = styled(FlexDivRow)`
 
 const DataContainer = styled(FlexDivColumn)``;
 
-const Heading = styled.h3`
+const Heading = styled.h3<{ highlightCard: boolean }>`
     font-weight: 700;
-    color: ${(_props) => _props.theme.textColor.primary};
+    color: ${(_props) => `${!_props.highlightCard ? _props.theme.textColor.primary : _props.theme.background.primary}`};
     text-transform: uppercase;
 `;
 
-const Amount = styled.span`
+const Amount = styled.span<{ highlightCard: boolean }>`
     font-weight: 700;
     text-transform: uppercase;
-    color: ${(_props) => _props.theme.button.textColor.quaternary};
+    color: ${(_props) =>
+        `${!_props.highlightCard ? _props.theme.button.textColor.quaternary : _props.theme.background.primary}`};
 `;
 
 const BadgeContainer = styled.div`
