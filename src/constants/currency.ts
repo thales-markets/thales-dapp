1d8059f505e3251e21834ebf8a8d3880c02d403e
Gorshtak <sg31191@gmail.com>
2023-08-01 17:50:28 +0200

    fixed coingecko id

--- src/constants/currency.ts
+++ src/constants/currency.ts
@@ -419,7 +419,7 @@ export const currencyKeyToCoinGeckoIndexMap = {
     [CRYPTO_CURRENCY_MAP.GMX]: 'gmx',
     [CRYPTO_CURRENCY_MAP.CAKE]: 'pancakeswap-token',
     [CRYPTO_CURRENCY_MAP.PEPE]: 'pepe',
-    [CRYPTO_CURRENCY_MAP.WLD]: 'worldcoin',
+    [CRYPTO_CURRENCY_MAP.WLD]: 'worldcoin-wld',
     [COMMODITY_MAP.XAU]: 'tether-gold',
     [COMMODITY_MAP.XAG]: 'kinesis-silver',
 };
