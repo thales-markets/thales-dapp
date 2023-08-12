eee2bc0c7113ff9920621e715eeced2c690afbd8
Ruben Tarin <95529720+Sanguine-Thales@users.noreply.github.com>
2023-08-12 16:32:50 +0900

    Fixed wizard collection names and rod position in fisherman

--- src/pages/TaleOfThales/components/Mint/constants.ts
+++ src/pages/TaleOfThales/components/Mint/constants.ts
@@ -52,10 +52,10 @@ export const NFT_COLLECTIONS = [
         name: 'Fisherman Collection',
         condition: '"Fisherman Collection" is unlocked for all participants in the Fishing Competition',
         items: [
-            { name: 'Rod', src: '/miletus-nfts/NFT_16.png', itemId: 17 },
             { name: 'Hat', src: '/miletus-nfts/NFT_17.png', itemId: 18 },
             { name: 'Robe', src: '/miletus-nfts/NFT_18.png', itemId: 19 },
             { name: 'Boots', src: '/miletus-nfts/NFT_19.png', itemId: 20 },
+            { name: 'Rod', src: '/miletus-nfts/NFT_16.png', itemId: 17 },
         ],
     },
     {
@@ -81,7 +81,7 @@ export const NFT_COLLECTIONS = [
     },
     {
         collectionId: 8,
-        name: 'COLLECTION APPRENTICE WIZARD',
+        name: 'Apprentice Wizard Collection',
         condition: '"Apprentice Wizard Collection" is unlocked for all participants in the Decoration Competition',
         items: [
             { name: 'Hat', src: '/miletus-nfts/NFT_26.png', itemId: 27 },
@@ -92,7 +92,7 @@ export const NFT_COLLECTIONS = [
     },
     {
         collectionId: 9,
-        name: 'COLLECTION ELDER WIZARD',
+        name: 'Elder Wizard Collection',
         condition: '"Elder Wizard Collection" is unlocked for the winners of the Decoration Competition',
         items: [
             { name: 'Hat', src: '/miletus-nfts/NFT_30.png', itemId: 31 },
