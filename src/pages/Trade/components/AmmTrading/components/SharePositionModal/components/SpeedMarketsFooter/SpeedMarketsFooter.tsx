fe480311107ef2f444f7274472220544001ea550
makiBaraba <markovic.ofsec@gmail.com>
2023-08-17 13:44:35 +0200

    Footer for SpeedMarkets

---
+++ src/pages/Trade/components/AmmTrading/components/SharePositionModal/components/SpeedMarketsFooter/SpeedMarketsFooter.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+import SpeedMarketsLogo from 'assets/images/flex-cards/speed_markets_logo.svg';
+import { useSelector } from 'react-redux';
+import { RootState } from 'redux/rootReducer';
+import useGetReffererIdQuery from 'queries/referral/useGetReffererIdQuery';
+import { getWalletAddress } from 'redux/modules/wallet';
+import { buildReferrerLink } from 'utils/routes';
+import QRCode from 'react-qr-code';
+import ROUTES from 'constants/routes';
+
+const SpeedMarketsFooter: React.FC = () => {
+    const walletAddress = useSelector((state: RootState) => getWalletAddress(state)) || '';
+
+    const reffererIDQuery = useGetReffererIdQuery(walletAddress || '', { enabled: !!walletAddress });
+    const reffererID = reffererIDQuery.isSuccess && reffererIDQuery.data ? reffererIDQuery.data : '';
+
+    return (
+        <Container>
+            <Logo src={SpeedMarketsLogo} />
+            {reffererID && (
+                <ReferralWrapper>
+                    <QRCode size={70} value={buildReferrerLink(ROUTES.Home, reffererID)} />
+                </ReferralWrapper>
+            )}
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+    height: 60px;
+    margin-bottom: 10px;
+`;
+
+const Logo = styled.img`
+    width: 70%;
+`;
+
+const ReferralWrapper = styled.div`
+    display: flex;
+    justify-content: flex-start;
+`;
+
+export default SpeedMarketsFooter;
