8f4b767643ac1ce1e356332f6f9f9bfc21ff862f
Gorshtak <sg31191@gmail.com>
2024-02-28 14:52:39 +0100

    merging multiple positions when they have same timestamp

--- src/pages/Trade/components/PriceChart/components/Chart/UserSeries/UserSeriesComponent.tsx
+++ src/pages/Trade/components/PriceChart/components/Chart/UserSeries/UserSeriesComponent.tsx
@@ -130,18 +130,78 @@ export const UserPositionAreaSeries: React.FC<{
             });
             series.setData(userDataWithLocalTime as any);
 
-            const markers = userDataWithLocalTime
+            const cleanArray = [];
+            // merging multiple positions that have the same timestamp in one marker
+            // checking if user has more than 1 active position
+            if (userDataWithLocalTime.length > 1) {
+                for (let index = 0; index < userDataWithLocalTime.length; index++) {
+                    // checking if for loop has reach the last user position
+                    if (index === userDataWithLocalTime.length - 1) {
+                        // if the last user position does not have the same timestmap as previous one we push it to array as circle
+                        if (userDataWithLocalTime[index - 1].time !== userDataWithLocalTime[index].time) {
+                            cleanArray.push({
+                                ...userDataWithLocalTime[index],
+                                shape: 'circle',
+                            });
+                        } else {
+                            // if the timestamp of last and prior to last positions match, then we push the last position as square
+                            cleanArray.push({
+                                ...userDataWithLocalTime[index],
+                                shape: 'square',
+                            });
+                        }
+                    } else if (userDataWithLocalTime[index].time === userDataWithLocalTime[index + 1].time) {
+                        // here we check if the user position timestamp is equal to the next item in the array, if so we just proceed to the next one
+                        continue;
+                    } else {
+                        // we reach the part where current position is different from the next one
+                        // we need to check if the current position is the first one in the array
+                        // if so we just push the position as there is no previous positions to check
+                        if (index === 0) {
+                            cleanArray.push({
+                                ...userDataWithLocalTime[index],
+                                shape: 'circle',
+                            });
+                        } else {
+                            // here we need to check previous position timestamp as we know that the next one does not match the current
+                            // if timestamp are different we can push position as circle
+                            if (userDataWithLocalTime[index - 1].time !== userDataWithLocalTime[index].time) {
+                                cleanArray.push({
+                                    ...userDataWithLocalTime[index],
+                                    shape: 'circle',
+                                });
+                            } else {
+                                // otherwise we need to mark this one as multiple positions by drawing a square
+                                cleanArray.push({
+                                    ...userDataWithLocalTime[index],
+                                    shape: 'square',
+                                });
+                            }
+                        }
+                    }
+                }
+            } else {
+                cleanArray.push(...userDataWithLocalTime);
+            }
+
+            const markers = cleanArray
                 .filter((value: any) => !value.hide)
                 .map((value: any) => {
                     return {
                         time: value.time,
                         position: 'inBar',
                         size: 0.1,
-                        color: value.position.side === Positions.UP ? Colors.GREEN : Colors.RED,
-                        shape: 'circle',
-                        text: value.position.side,
+                        color:
+                            value.shape === 'square'
+                                ? Colors.PURPLE
+                                : value.position.side === Positions.UP
+                                ? Colors.GREEN
+                                : Colors.RED,
+                        shape: value.shape,
+                        text: value.shape === 'square' ? 'Multi positions' : value.position.side,
                     };
                 });
+
             series?.setMarkers(markers as any);
         } else {
             series?.setMarkers([]);
