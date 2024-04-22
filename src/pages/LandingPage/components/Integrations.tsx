4ba22de6ca8e36330fadbf8bd46e04778a99d7c5
makiBaraba <markovic.ofsec@gmail.com>
2024-04-22 13:27:54 +0200

    Remove Polygon/Pyth/Products buttons

--- src/pages/LandingPage/components/Integrations.tsx
+++ src/pages/LandingPage/components/Integrations.tsx
@@ -1,10 +1,6 @@
-import SPAAnchor from 'components/SPAAnchor';
-import { LINKS } from 'constants/links';
-import ROUTES from 'constants/routes';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
-import { buildHref } from 'utils/routes';
 import { FlexDiv, FlexWrapper, Title } from './styled-components';
 
 const Integrations: React.FC = () => {
@@ -12,27 +8,6 @@ const Integrations: React.FC = () => {
 
     return (
         <FlexWrapper>
-            <Title> {t('landing-page.products')}</Title>
-            <ButtonWrapper>
-                <SPAAnchor href={buildHref(ROUTES.Options.Home)}>
-                    <Button>
-                        {t('landing-page.product-links.digital-options')}
-                        <Arrow className="icon icon--right" />
-                    </Button>
-                </SPAAnchor>
-                <SPAAnchor href={buildHref(ROUTES.Options.RangeMarkets)}>
-                    <Button>
-                        {t('landing-page.product-links.ranged-markets')}
-                        <Arrow className="icon icon--right" />
-                    </Button>
-                </SPAAnchor>
-                <SPAAnchor href={buildHref(LINKS.SpeedMarkets)}>
-                    <Button>
-                        {t('landing-page.product-links.speed-markets')}
-                        <Arrow className="icon icon--right" />
-                    </Button>
-                </SPAAnchor>
-            </ButtonWrapper>
             <Title> {t('landing-page.supported-networks')}</Title>
             <SupportedNetworkWrapper>
                 <a target="_blank" rel="noreferrer" href="https://www.optimism.io/">
@@ -43,9 +18,6 @@ const Integrations: React.FC = () => {
                 </a>
                 <a target="_blank" rel="noreferrer" href="https://base.org/">
                     <BASE className="icon-home icon-home--base" />
-                </a>
-                <a target="_blank" rel="noreferrer" href="https://base.org/">
-                    <POLYGON className="icon-home icon-home--polygon" />
                 </a>
             </SupportedNetworkWrapper>
             <Title>{t('landing-page.infrastructure')}</Title>
@@ -58,9 +30,6 @@ const Integrations: React.FC = () => {
                 </a>
                 <a target="_blank" rel="noreferrer" href="https://chain.link/">
                     <LINK className="icon-home icon-home--link" />
-                </a>
-                <a target="_blank" rel="noreferrer" href="https://pyth.network/">
-                    <PYTH className="icon-home icon-home--pyth" />
                 </a>
             </InfrastuctureWrapper>
         </FlexWrapper>
@@ -137,68 +106,12 @@ const BASE = styled(IconAbs)`
     }
 `;
 
-const POLYGON = styled(IconAbs)`
-    font-size: 15em;
-    line-height: 0.5em;
-    margin-bottom: -20px;
-    @media (max-width: 600px) {
-        font-size: 8em;
-        margin-bottom: -15px;
-    }
-`;
-
 const LINK = styled(IconAbs)`
     font-size: 14em;
     line-height: 0.5em;
     @media (max-width: 600px) {
         font-size: 10em;
     }
-`;
-
-const PYTH = styled(IconAbs)`
-    font-size: 12em;
-    line-height: 0.5em;
-    margin-bottom: -30px;
-    @media (max-width: 600px) {
-        font-size: 8em;
-    }
-`;
-
-const ButtonWrapper = styled(FlexDiv)`
-    margin: 50px 0 90px 0;
-    align-items: center;
-    justify-content: center;
-`;
-
-const Button = styled.div`
-    background-color: ${(props) => props.theme.landingPage.button.background.primary};
-    color: ${(props) => props.theme.landingPage.button.textColor.primary} !important;
-    border-radius: 32px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 42px;
-    z-index: 2;
-    margin: 0 20px;
-    padding: 25px 30px;
-    font-weight: 800;
-    cursor: pointer;
-    font-size: 22px;
-    a {
-        color: ${(props) => props.theme.landingPage.button.textColor.primary} !important;
-    }
-    i {
-        color: ${(props) => props.theme.landingPage.button.textColor.primary} !important;
-    }
-    @media (max-width: 800px) {
-        margin-bottom: 20px;
-    }
-`;
-
-const Arrow = styled.i`
-    font-family: Icons !important;
-    font-weight: bold;
-    margin-left: 10px;
 `;
 
 export default Integrations;
