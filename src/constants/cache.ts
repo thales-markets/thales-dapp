585cf7c0ccbcc429afcddaddb28d06b32f2df3a3
makiBaraba <markovic.ofsec@gmail.com>
2024-05-22 13:37:25 +0200

    Cache keys

---
+++ src/constants/cache.ts
@@ -0,0 +1,20 @@
+export const CACHE_PREFIX_KEYS = {
+    Stakers: 'stakers-',
+    ClaimOnBehalfItems: 'claim-on-behalf',
+    TokenTransactions: 'token-transactions',
+    LiquidityPoolPnl: 'liquidity-pool-pnl-',
+    LiquidityPoolTransactions: 'liquidity-pool-tx-',
+    Markets: 'markets-',
+    Trades: 'trades-',
+    OptionTransactions: 'option-transactions-',
+    RangedMarkets: 'ranged-markets',
+    PositionBalance: 'position-balance',
+    RangePositionBalance: 'range-position-balance',
+    ReferralTransactions: 'referral-transactions',
+    ReferredTraders: 'referred-traders',
+    Referrers: 'referrers',
+
+    VaultUserTransactions: 'vault-user-transactions',
+    VaultPnl: 'vault-pnl',
+    VaultTransactions: 'vault-transactions',
+};
