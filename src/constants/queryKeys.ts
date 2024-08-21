9330d210b26b3262096a4dcd8f8dc3c4dae77f22
Sasa Lotina <lotinasa@gmail.com>
2024-08-21 12:56:54 +0200

    Remove Referral page and modal

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -162,25 +162,6 @@ const QUERY_KEYS = {
         Swap: (networkId: Network) => ['swap', 'swap', networkId],
     },
     Referral: {
-        Referrer: (networkId: Network, address?: string) => [
-            'referral',
-            'referrer',
-            networkId,
-            address ? address : undefined,
-        ],
-        ReferredTrader: (networkId: Network, referrer?: string) => [
-            'referral',
-            'ReferredTrader',
-            networkId,
-            referrer ? referrer : undefined,
-        ],
-        ReferralTransacations: (networkId: Network, trader?: string, refferer?: string) => [
-            'referral',
-            'referralTransacations',
-            networkId,
-            trader ? trader : undefined,
-            refferer ? refferer : undefined,
-        ],
         ReferrerID: (walletAddress: string) => ['referrerId', walletAddress],
     },
     Bungee: {
